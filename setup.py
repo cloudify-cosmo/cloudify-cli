bae638acad82e50d152d755a314c198b8305d59a
Ran <ran@gigaspaces.com>
2014-01-22 16:32:21 +0200

    added dsl parser to setup.py dependencies

--- setup.py
+++ setup.py
@@ -26,6 +26,13 @@ COSMO_MANAGER_REST_CLIENT = \
     "0}#egg=cosmo-manager-rest-client-{1}".format(
         COSMO_MANAGER_REST_CLIENT_BRANCH, COSMO_MANAGER_REST_CLIENT_VERSION)
 
+COSMO_PLUGIN_DSL_PARSER_VERSION = '0.3'
+COSMO_PLUGIN_DSL_PARSER_BRANCH = 'develop'
+COSMO_PLUGIN_DSL_PARSER = \
+    "https://github.com/CloudifySource/cosmo-plugin-dsl-parser/tarball/{" \
+    "0}#egg=cosmo-plugin-dsl-parser-{1}".format(
+        COSMO_PLUGIN_DSL_PARSER_BRANCH, COSMO_PLUGIN_DSL_PARSER_VERSION)
+
 setup(
     name='cosmo-cli',
     version=version,
@@ -38,8 +45,7 @@ setup(
         'console_scripts': ['cfy = cosmo_cli.cosmo_cli:main']
     },
     install_requires=[
-        "pyyaml",
-        "cosmo-manager-rest-client"
+        "pyyaml"
     ],
-    dependency_links=[COSMO_MANAGER_REST_CLIENT]
+    dependency_links=[COSMO_MANAGER_REST_CLIENT, COSMO_PLUGIN_DSL_PARSER]
 )
