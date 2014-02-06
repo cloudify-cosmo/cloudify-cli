c76c7e90557172a806da8bc8a3659323ba091107
iliapolo <elip@gigaspaces.com>
2014-02-06 20:49:08 +0200

    CLOUDIFY-2370 added log

--- cosmo_cli/cosmo_cli.py
+++ cosmo_cli/cosmo_cli.py
@@ -853,7 +853,7 @@ def _get_resource_base():
         resource_directory_url = urlparse.urljoin('file:', urllib.pathname2url(
             resource_directory))
         return resource_directory_url
-    _output(logging.debug, "Using resources from github")
+    _output(logging.debug, "Using resources from github. Branch is feature/CLOUDIFY-2370-plugins-as-python-libs")
     return "https://raw.github.com/CloudifySource/cosmo-manager/feature/CLOUDIFY-2370-plugins-as-python-libs/" \
            "orchestrator/src/main/resources/"
 
