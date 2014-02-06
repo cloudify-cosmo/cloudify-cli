b1768b26fde334fae5f238a33738cf709f7c45e1
iliapolo <elip@gigaspaces.com>
2014-02-06 18:53:22 +0200

    CLOUDIFY-2370 fix plugin refs

--- cosmo_cli/cosmo_cli.py
+++ cosmo_cli/cosmo_cli.py
@@ -854,7 +854,7 @@ def _get_resource_base():
             resource_directory))
         return resource_directory_url
     _output(logging.debug, "Using resources from github")
-    return "https://raw.github.com/CloudifySource/cosmo-manager/develop/" \
+    return "https://raw.github.com/CloudifySource/cosmo-manager/feature/CLOUDIFY-2370-plugins-as-python-libs/" \
            "orchestrator/src/main/resources/"
 
 
