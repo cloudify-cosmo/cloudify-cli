9d4a468fc9d2a68e54fddd462974924e90c63695
nir0s <nir36g@gmail.com>
2014-03-06 13:37:03 +0200

    CLOUDIFY-2410 removed unnecessary code

--- setup.py
+++ setup.py
@@ -13,11 +13,6 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
-# activate venv
-# pip install argcomplete
-# eval "$(register-python-argcomplete cfy)"
-# echo 'eval "$(register-python-argcomplete cfy)"' >> ~/.bashrc
-
 __author__ = 'ran'
 
 from setuptools import setup
