fa0c2363f88c43c47e23ff7d8d960ac0e0d3c655
yaelpeled <yael@gigaspaces.com>
2015-03-18 11:52:33 +0200

    CFY-2353 added print to debug

--- cloudify_cli/tests/test_secured_get_rest_client.py
+++ cloudify_cli/tests/test_secured_get_rest_client.py
@@ -28,6 +28,9 @@ class TestGetRestClient(unittest.TestCase):
         os.environ[CLOUDIFY_PASSWORD_ENV] = 'test_password'
 
     def test_get_rest_client(self):
+        self.assertIsNotNone(os.environ[CLOUDIFY_USERNAME_ENV],
+                             'expected {0} to be test_username but it is None'
+                             .format(CLOUDIFY_USERNAME_ENV))
         client = utils.get_rest_client(
             manager_ip='localhost', rest_port=80)
         self.assertIsNotNone(client._client.encoded_credentials)
