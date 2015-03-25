64370f811ef03706df0347c09c179b311902d438
opencm <limor@gigaspaces.com>
2015-03-25 12:06:37 +0200

    Bump version to 3.2m8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify',
-    version='3.2a7',
+    version='3.2a8',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['cloudify_cli',
@@ -37,10 +37,10 @@ setup(
         ]
     },
     install_requires=[
-        'cloudify-plugins-common==3.2a7',
-        'cloudify-rest-client==3.2a7',
-        'cloudify-dsl-parser==3.2a7',
-        'cloudify-script-plugin==1.2a7',
+        'cloudify-plugins-common==3.2a8',
+        'cloudify-rest-client==3.2a8',
+        'cloudify-dsl-parser==3.2a8',
+        'cloudify-script-plugin==1.2a8',
         'pyyaml==3.10',
         'argcomplete==0.7.1',
         'fabric==1.8.3',
