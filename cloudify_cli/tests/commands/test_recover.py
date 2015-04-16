bdf615f5ab43fa77f41f386e7598bc8c6c8a6de2
Eli Polonsky <elip@gigaspaces.com>
2015-04-16 14:36:48 +0300

    Merge pull request #159 from cloudify-cosmo/CFY-2473-recovery-fix
    Cfy 2473 recovery fix
    Conflicts:
    	cloudify_cli/commands/recover.py

--- cloudify_cli/tests/commands/test_recover.py
+++ cloudify_cli/tests/commands/test_recover.py
@@ -17,9 +17,15 @@
 Tests 'cfy recover'
 """
 
+import os
+
+from cloudify_cli.utils import update_wd_settings
+
 from mock import MagicMock
+from mock import patch
 from cloudify_cli.tests import cli_runner
 from cloudify_cli.tests.commands.test_cli_command import CliCommandTest
+from cloudify_cli.tests.commands.test_cli_command import TEST_WORK_DIR
 
 
 class RecoverTest(CliCommandTest):
@@ -34,3 +40,95 @@ class RecoverTest(CliCommandTest):
         cli_runner.run_cli('cfy use -t 10.0.0.1')
         self._assert_ex('cfy recover',
                         'This action requires additional confirmation.')
+
+    @patch('cloudify_cli.bootstrap.bootstrap'
+           '.read_manager_deployment_dump_if_needed')
+    @patch('cloudify_cli.bootstrap.bootstrap.recover')
+    def test_recover_from_same_directory_as_bootstrap(self, *_):
+        cli_runner.run_cli('cfy init')
+
+        # mock bootstrap behavior by setting the management key path
+        # in the local context
+        key_path = os.path.join(TEST_WORK_DIR, 'key.pem')
+        open(key_path, 'w').close()
+
+        with update_wd_settings() as wd:
+            wd.set_management_key(key_path)
+            wd.set_provider_context({})
+
+        # now run recovery and make sure no exception was raised
+        cli_runner.run_cli('cfy recover -f')
+
+    @patch('cloudify_cli.bootstrap.bootstrap'
+           '.read_manager_deployment_dump_if_needed')
+    @patch('cloudify_cli.bootstrap.bootstrap.recover')
+    def test_recover_from_same_directory_as_bootstrap_missing_key(self, *_):
+        cli_runner.run_cli('cfy init')
+
+        # mock bootstrap behavior by setting the management key path
+        # in the local context. however, don't actually create the key file
+        key_path = os.path.join(TEST_WORK_DIR, 'key.pem')
+
+        with update_wd_settings() as wd:
+            wd.set_management_key(key_path)
+            wd.set_provider_context({})
+
+        # recovery command should not fail because the key file specified in
+        # the context file does not exist
+        self._assert_ex('cfy recover -f',
+                        'Cannot perform recovery. manager key '
+                        'file does not exist')
+
+    @patch('cloudify_cli.bootstrap.bootstrap'
+           '.read_manager_deployment_dump_if_needed')
+    @patch('cloudify_cli.bootstrap.bootstrap.recover')
+    def test_recover_missing_key_with_env(self, *_):
+        cli_runner.run_cli('cfy init')
+
+        key_path = os.path.join(TEST_WORK_DIR, 'key.pem')
+        try:
+            os.environ['CLOUDIFY_MANAGER_PRIVATE_KEY_PATH'] = key_path
+
+            # recovery command should not fail because the key file
+            # specified in the context file does not exist
+            self._assert_ex('cfy recover -f',
+                            'Cannot perform recovery. manager private '
+                            'key file defined in '
+                            'CLOUDIFY_MANAGER_PRIVATE_KEY_PATH '
+                            'environment variable does not exist: '
+                            '{0}'.format(key_path))
+        finally:
+            del os.environ['CLOUDIFY_MANAGER_PRIVATE_KEY_PATH']
+
+    @patch('cloudify_cli.bootstrap.bootstrap'
+           '.read_manager_deployment_dump_if_needed')
+    @patch('cloudify_cli.bootstrap.bootstrap.recover')
+    def test_recover_from_different_directory_than_bootstrap(self, *_):
+        cli_runner.run_cli('cfy init')
+        # recovery command should not fail because we do not have a manager
+        # key path in the local context, and the environment variable is not
+        # set
+        self._assert_ex('cfy recover -f',
+                        'Cannot perform recovery. manager key file not found. '
+                        'Set the manager private key path via the '
+                        'CLOUDIFY_MANAGER_PRIVATE_KEY_PATH environment '
+                        'variable')
+
+    @patch('cloudify_cli.bootstrap.bootstrap'
+           '.read_manager_deployment_dump_if_needed')
+    @patch('cloudify_cli.bootstrap.bootstrap.recover')
+    def test_recover_from_different_directory_than_bootstrap_with_env_variable(self, *_):  # NOQA
+        cli_runner.run_cli('cfy init')
+        # recovery command should not fail because we do not have a manager
+        # key path in the local context, and the environment variable is not
+        # set
+
+        # mock bootstrap behavior by setting the management key path
+        # in the local context
+        key_path = os.path.join(TEST_WORK_DIR, 'key.pem')
+        open(key_path, 'w').close()
+        try:
+            os.environ['CLOUDIFY_MANAGER_PRIVATE_KEY_PATH'] = key_path
+            cli_runner.run_cli('cfy recover -f')
+        finally:
+            del os.environ['CLOUDIFY_MANAGER_PRIVATE_KEY_PATH']
