a3b3dd244b2fda08c58ab086aad304802f6fc36a
opencm <limor@gigaspaces.com>
2015-09-27 11:15:37 +0300

    Bump version to 3.3m6

--- packaging/windows/provision.sh
+++ packaging/windows/provision.sh
@@ -42,10 +42,10 @@ function upload_to_s3() {
 
 # VERSION/PRERELEASE/BUILD must be exported as they is being read as an env var by the install wizard
 export VERSION="3.3.0"
-export PRERELEASE="m5"
-export BUILD="275"
-CORE_TAG_NAME="3.3m5"
-PLUGINS_TAG_NAME="1.3m5"
+export PRERELEASE="m6"
+export BUILD="276"
+CORE_TAG_NAME="3.3m6"
+PLUGINS_TAG_NAME="1.3m6"
 
 AWS_ACCESS_KEY_ID=$1
 AWS_ACCESS_KEY=$2
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify',
-    version='3.3a5',
+    version='3.3a6',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['cloudify_cli',
@@ -43,10 +43,10 @@ setup(
         ]
     },
     install_requires=[
-        'cloudify-plugins-common==3.3a5',
-        'cloudify-rest-client==3.3a5',
-        'cloudify-dsl-parser==3.3a5',
-        'cloudify-script-plugin==1.3a5',
+        'cloudify-plugins-common==3.3a6',
+        'cloudify-rest-client==3.3a6',
+        'cloudify-dsl-parser==3.3a6',
+        'cloudify-script-plugin==1.3a6',
         'pyyaml==3.10',
         'argcomplete==0.7.1',
         'fabric==1.8.3',
