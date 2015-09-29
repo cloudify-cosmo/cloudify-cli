f59b08a21e79f613833e096a67ff6ba60b754404
Marcin Wasilewski <marcin.wasilewski@codilime.com>
2015-09-29 14:49:14 +0200

    CFY-3036 Fixes after review.

--- cloudify_cli/commands/agents.py
+++ cloudify_cli/commands/agents.py
@@ -22,118 +22,116 @@ from cloudify_cli.exceptions import SuppressedCloudifyCliError
 from cloudify_cli.exceptions import ExecutionTimeoutError
 from cloudify import logs
 
+_NODE_INSTANCE_STATE_STARTED = 'started'
+
+
+def _is_deployment_installed(client, deployment_id):
+    for node_instance in client.node_instances.list(deployment_id):
+        if node_instance.state != _NODE_INSTANCE_STATE_STARTED:
+            return False
+    return True
+
+
+def _deployment_exists(client, deployment_id):
+    try:
+        client.deployments.get(deployment_id)
+    except:
+        return False
+    return True
+
 
 def install(deployment_id, include_logs):
     workflow_id = 'install_new_agents'
     logger = get_logger()
-    management_ip = utils.get_management_server_ip()
-    client = utils.get_rest_client(management_ip)
+    client = utils.get_rest_client()
 
     if deployment_id:
+        deps = [deployment_id]
+        if not _deployment_exists(client, deployment_id):
+            logger.info("Deployment '{0}' does not exist"
+                        .format(deployment_id))
+            raise SuppressedCloudifyCliError()
+        if not _is_deployment_installed(client, deployment_id):
+            logger.info("Deployment '{0}' is not installed"
+                        .format(deployment_id))
+            raise SuppressedCloudifyCliError()
         logger.info("Installing agent for deployment '{0}'"
                     .format(deployment_id))
+    else:
+        deps = [dep.id for dep in client.deployments.list()
+                if _is_deployment_installed(client, dep.id)]
+        if not deps:
+            logger.info('There are no deployments installed')
+            raise SuppressedCloudifyCliError()
+        logger.info('Installing agents for all installed deployments')
+
+    error_summary = []
+    error_summary_lock = threading.Lock()
+
+    event_lock = threading.Lock()
+
+    def log_to_summary(message):
+        with error_summary_lock:
+            error_summary.append(message)
+
+    def threadsafe_log(message):
+        with event_lock:
+            logger.info(message)
+
+    def threadsafe_events_logger(events):
+        with event_lock:
+            for event in events:
+                logger.info(logs.create_event_message_prefix(event))
+
+    def worker(dep_id):
         try:
             execution = client.executions.start(
-                deployment_id,
+                dep_id,
                 workflow_id,
             )
 
             execution = wait_for_execution(
                 client,
                 execution,
-                events_handler=get_events_logger(),
+                events_handler=threadsafe_events_logger,
                 include_logs=include_logs,
                 timeout=900
             )
 
             if execution.error:
-                logger.info("Execution of workflow '{0}' for "
-                            "deployment '{1}' failed. [error={2}]"
-                            .format(workflow_id, deployment_id,
-                                    execution.error))
+                log_to_summary("Execution of workflow '{0}' for "
+                               "deployment '{1}' failed. [error={2}]"
+                               .format(workflow_id,
+                                       dep_id,
+                                       execution.error))
             else:
-                logger.info("Finished executing workflow "
-                            "'{0}' on deployment"
-                            " '{1}'".format(workflow_id, deployment_id))
+                threadsafe_log("Finished executing workflow "
+                               "'{0}' on deployment"
+                               " '{1}'".format(workflow_id, dep_id))
 
         except ExecutionTimeoutError as e:
-            logger.info("Execution of workflow '{0}' "
-                        "for deployment '{1}' timed out. "
-                        "* Run 'cfy executions cancel "
-                        "--execution-id {2}' to cancel"
-                        " the running workflow."
-                        .format(workflow_id, deployment_id, e.execution_id))
+            log_to_summary("Execution of workflow '{0}' "
+                           "for deployment '{1}' timed out. "
+                           "* Run 'cfy executions cancel "
+                           "--execution-id {2}' to cancel"
+                           " the running workflow."
+                           .format(
+                                workflow_id,
+                                deployment_id,
+                                e.execution_id
+                           ))
 
-            raise SuppressedCloudifyCliError()
-    else:
-        logger.info('Installing agents for all deployments')
-        deps = client.deployments.list()
+    threads = [threading.Thread(target=worker, args=(dep_id,))
+               for dep_id in deps]
 
-        error_summary = []
-        error_summary_lock = threading.Lock()
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
 
-        event_lock = threading.Lock()
+    if error_summary:
+        logger.info('Summary:\n{0}\n'.format(
+            '\n'.join(error_summary)
+        ))
 
-        def log_to_summary(message):
-            with error_summary_lock:
-                error_summary.append(message)
-
-        def threadsafe_log(message):
-            with event_lock:
-                logger.info(message)
-
-        def threadsafe_events_logger(events):
-            with event_lock:
-                for event in events:
-                    logger.info(logs.create_event_message_prefix(event))
-
-        def worker(dep_id):
-            try:
-                execution = client.executions.start(
-                    dep_id,
-                    workflow_id,
-                )
-
-                execution = wait_for_execution(
-                    client,
-                    execution,
-                    events_handler=threadsafe_events_logger,
-                    include_logs=include_logs,
-                    timeout=900
-                )
-
-                if execution.error:
-                    log_to_summary("Execution of workflow '{0}' for "
-                                   "deployment '{1}' failed. [error={2}]"
-                                   .format(workflow_id,
-                                           dep_id,
-                                           execution.error))
-                else:
-                    threadsafe_log("Finished executing workflow "
-                                   "'{0}' on deployment"
-                                   " '{1}'".format(workflow_id, dep_id))
-
-            except ExecutionTimeoutError as e:
-                log_to_summary("Execution of workflow '{0}' "
-                               "for deployment '{1}' timed out. "
-                               "* Run 'cfy executions cancel "
-                               "--execution-id {2}' to cancel"
-                               " the running workflow."
-                               .format(
-                                    workflow_id,
-                                    deployment_id,
-                                    e.execution_id
-                               ))
-
-        threads = [threading.Thread(target=worker, args=(dep.id,))
-                   for dep in deps]
-
-        for t in threads:
-            t.start()
-        for t in threads:
-            t.join()
-
-        if error_summary:
-            logger.info('Summary:\n{0}\n'.format(
-                '\n'.join(error_summary)
-            ))
+        raise SuppressedCloudifyCliError()
