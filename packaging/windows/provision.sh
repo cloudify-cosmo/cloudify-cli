045ea5a8fa0a13800f62a227f0573901aff0a746
max-orlov <mxmrlv@gmail.com>
2015-11-09 13:15:58 +0200

    CFY-4018-Adding-necessary-resources-to-windows-cli-package-rc1build

--- packaging/windows/provision.sh
+++ packaging/windows/provision.sh
@@ -7,29 +7,79 @@ function install_requirements() {
 }
 
 function download_wheels() {
-    pip wheel --wheel-dir packaging/source/wheels https://github.com/cloudify-cosmo/cloudify-cli/archive/$CORE_TAG_NAME.zip#egg=cloudify-cli \
+    GITHUB_USERNAME=$1
+    GITHUB_PASSWORD=$2
+
+    curl -LO https://pypi.python.org/packages/2.7/l/lxml/lxml-3.5.0.win32-py2.7.exe
+    wheel convert lxml-3.5.0.win32-py2.7.exe --dest-dir packaging/source/wheels
+    pip install lxml-*.whl
+
+    pip wheel --wheel-dir packaging/source/wheels --find-links packaging/source/wheels https://github.com/cloudify-cosmo/cloudify-cli/archive/$CORE_TAG_NAME.zip#egg=cloudify-cli \
     https://github.com/cloudify-cosmo/cloudify-rest-client/archive/$CORE_TAG_NAME.zip#egg=cloudify-rest-client \
     https://github.com/cloudify-cosmo/cloudify-dsl-parser/archive/$CORE_TAG_NAME.zip#egg=cloudify-dsl-parser \
     https://github.com/cloudify-cosmo/cloudify-plugins-common/archive/$CORE_TAG_NAME.zip#egg=cloudify-plugins-common \
-    https://github.com/cloudify-cosmo/cloudify-script-plugin/archive/$PLUGINS_TAG_NAME.zip#egg=cloudify-script-plugin
+    https://github.com/cloudify-cosmo/cloudify-script-plugin/archive/$PLUGINS_TAG_NAME.zip#egg=cloudify-script-plugin \
+    https://github.com/cloudify-cosmo/cloudify-fabric-plugin/archive/$PLUGINS_TAG_NAME.zip#egg=cloudify-fabric-plugin \
+    https://github.com/cloudify-cosmo/cloudify-openstack-plugin/archive/$PLUGINS_TAG_NAME.zip#egg=cloudify-openstack-plugin \
+    https://github.com/cloudify-cosmo/cloudify-aws-plugin/archive/$PLUGINS_TAG_NAME.zip#egg=cloudify-aws-plugin \
+    https://github.com/cloudify-cosmo/tosca-vcloud-plugin/archive/$PLUGINS_TAG_NAME.zip#egg=tosca-vcloud-plugin \
+    https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/cloudify-cosmo/cloudify-vsphere-plugin/archive/$PLUGINS_TAG_NAME.zip#egg=cloudify-vsphere-plugin \
+    https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/cloudify-cosmo/cloudify-softlayer-plugin/archive/$PLUGINS_TAG_NAME.zip#egg=cloudify-softlayer-plugin
 }
 
 function download_resources() {
-    mkdir -p packaging/source/{pip,python,virtualenv,blueprints}
+
+    GITHUB_USERNAME=$1
+    GITHUB_PASSWORD=$2
+
+    mkdir -p packaging/source/{pip,python,virtualenv,blueprints,types,scripts,plugins}
     pushd packaging/source/pip
-    curl -O https://dl.dropboxusercontent.com/u/407576/cfy-win-cli-package-resources/pip/get-pip.py
-    curl -O https://dl.dropboxusercontent.com/u/407576/cfy-win-cli-package-resources/pip/pip-6.1.1-py2.py3-none-any.whl
-    curl -O https://dl.dropboxusercontent.com/u/407576/cfy-win-cli-package-resources/pip/setuptools-15.2-py2.py3-none-any.whl
+        curl -LO https://dl.dropboxusercontent.com/u/407576/cfy-win-cli-package-resources/pip/get-pip.py
+        curl -LO https://dl.dropboxusercontent.com/u/407576/cfy-win-cli-package-resources/pip/pip-6.1.1-py2.py3-none-any.whl
+        curl -LO https://dl.dropboxusercontent.com/u/407576/cfy-win-cli-package-resources/pip/setuptools-15.2-py2.py3-none-any.whl
     popd
     pushd packaging/source/python
-    curl -O https://dl.dropboxusercontent.com/u/407576/cfy-win-cli-package-resources/python/python.msi
+        curl -LO https://dl.dropboxusercontent.com/u/407576/cfy-win-cli-package-resources/python/python.msi
     popd
     pushd packaging/source/virtualenv
-    curl -O https://dl.dropboxusercontent.com/u/407576/cfy-win-cli-package-resources/virtualenv/virtualenv-12.1.1-py2.py3-none-any.whl
+        curl -LO https://dl.dropboxusercontent.com/u/407576/cfy-win-cli-package-resources/virtualenv/virtualenv-12.1.1-py2.py3-none-any.whl
     popd
     pushd packaging/source/blueprints
-    curl --fail http://cloudify-public-repositories.s3.amazonaws.com/cloudify-manager-blueprints/$CORE_TAG_NAME/cloudify-manager-blueprints.tar.gz -o /tmp/cloudify-manager-blueprints.tar.gz
-    tar -zxvf /tmp/cloudify-manager-blueprints.tar.gz --strip-components=1
+        curl --fail http://cloudify-public-repositories.s3.amazonaws.com/cloudify-manager-blueprints/$CORE_TAG_NAME/cloudify-manager-blueprints.tar.gz -o /tmp/cloudify-manager-blueprints.tar.gz
+        tar -zxvf /tmp/cloudify-manager-blueprints.tar.gz --strip-components=1
+    popd
+
+    # Downloading types.yaml
+    pushd packaging/source/types
+        curl -LO http://getcloudify.org.s3.amazonaws.com/spec/cloudify/$CORE_TAG_NAME/types.yaml
+    popd
+
+    # Downloading Scripts
+    pushd packaging/source/scripts
+        curl -LO https://raw.githubusercontent.com/cloudify-cosmo/cloudify-manager/$CORE_TAG_NAME/resources/rest-service/cloudify/fs/mkfs.sh
+        curl -LO https://raw.githubusercontent.com/cloudify-cosmo/cloudify-manager/$CORE_TAG_NAME/resources/rest-service/cloudify/fs/fdisk.sh
+        curl -LO https://raw.githubusercontent.com/cloudify-cosmo/cloudify-manager/$CORE_TAG_NAME/resources/rest-service/cloudify/fs/mount.sh
+        curl -LO https://raw.githubusercontent.com/cloudify-cosmo/cloudify-manager/$CORE_TAG_NAME/resources/rest-service/cloudify/fs/unmount.sh
+        curl -LO https://raw.githubusercontent.com/cloudify-cosmo/cloudify-manager/$CORE_TAG_NAME/resources/rest-service/cloudify/policies/host_failure.clj
+        curl -LO https://raw.githubusercontent.com/cloudify-cosmo/cloudify-manager/$CORE_TAG_NAME/resources/rest-service/cloudify/policies/threshold.clj
+        curl -LO https://raw.githubusercontent.com/cloudify-cosmo/cloudify-manager/$CORE_TAG_NAME/resources/rest-service/cloudify/policies/ewma_stabilized.clj
+        curl -LO https://raw.githubusercontent.com/cloudify-cosmo/cloudify-manager/$CORE_TAG_NAME/resources/rest-service/cloudify/triggers/execute_workflow.clj
+    popd
+
+    # Downloading plugin yamls
+    pushd packaging/source/plugins
+        mkdir -p {fabric-plugin,script-plugin,diamond-plugin,openstack-plugin,aws-plugin,tosca-vcloud-plugin,vsphere-plugin,softlayer-plugin}
+
+        curl -L https://raw.githubusercontent.com/cloudify-cosmo/cloudify-fabric-plugin/$PLUGINS_TAG_NAME/plugin.yaml -o fabric-plugin/plugin.yaml
+        curl -L https://raw.githubusercontent.com/cloudify-cosmo/cloudify-script-plugin/$PLUGINS_TAG_NAME/plugin.yaml -o script-plugin/plugin.yaml
+        curl -L https://raw.githubusercontent.com/cloudify-cosmo/cloudify-diamond-plugin/$PLUGINS_TAG_NAME/plugin.yaml -o diamond-plugin/plugin.yaml
+        curl -L https://raw.githubusercontent.com/cloudify-cosmo/cloudify-openstack-plugin/$PLUGINS_TAG_NAME/plugin.yaml -o openstack-plugin/plugin.yaml
+        curl -L https://raw.githubusercontent.com/cloudify-cosmo/cloudify-aws-plugin/$PLUGINS_TAG_NAME/plugin.yaml -o aws-plugin/plugin.yaml
+        curl -L https://raw.githubusercontent.com/cloudify-cosmo/tosca-vcloud-plugin/$PLUGINS_TAG_NAME/plugin.yaml -o tosca-vcloud-plugin/plugin.yaml
+
+        # Downloading commercial plugin yamls
+        curl -L https://$GITHUB_USERNAME:$GITHUB_PASSWORD@raw.githubusercontent.com/cloudify-cosmo/cloudify-vsphere-plugin/1.3rc1/plugin.yaml -o vsphere-plugin/plugin.yaml
+        curl -L https://$GITHUB_USERNAME:$GITHUB_PASSWORD@raw.githubusercontent.com/cloudify-cosmo/cloudify-softlayer-plugin/1.3rc1/plugin.yaml -o softlayer-plugin/plugin.yaml
     popd
 }
 
@@ -55,16 +105,22 @@ function upload_to_s3() {
       "https://$AWS_S3_BUCKET.s3.amazonaws.com/$AWS_S3_PATH/$file"
 }
 
+function update_remote_to_local_links() {
+    sed -i -e 's/https:\/\/raw\.githubusercontent\.com\/cloudify-cosmo\/cloudify-manager\/.*\/resources\/rest-service\/cloudify\/.*\//file:\/cfy\/cloudify\/scripts\//g' packaging/source/types/types.yaml
+}
 
-# VERSION/PRERELEASE/BUILD must be exported as they are being read as an env var by the install wizard
+# VERSION/PRERELEASE/BUILD/CORE_TAG_NAME/PLUGINS_TAG_NAME must be exported as they are being read as an env var by the install wizard
 export VERSION="3.3.0"
 export PRERELEASE="rc1"
 export BUILD="278"
 CORE_TAG_NAME="3.3rc1"
 PLUGINS_TAG_NAME="1.3rc1"
 
-AWS_ACCESS_KEY_ID=$1
-AWS_ACCESS_KEY=$2
+GITHUB_USERNAME=$1
+GITHUB_PASSWORD=$2
+
+AWS_ACCESS_KEY_ID=$3
+AWS_ACCESS_KEY=$4
 AWS_S3_BUCKET="gigaspaces-repository-eu"
 AWS_S3_PATH="org/cloudify3/${VERSION}/${PRERELEASE}-RELEASE"
 
@@ -80,8 +136,9 @@ echo "AWS_S3_PATH: ${AWS_S3_PATH}"
 
 
 install_requirements &&
-download_wheels &&
-download_resources &&
+download_wheels $GITHUB_USERNAME $GITHUB_PASSWORD &&
+download_resources $GITHUB_USERNAME $GITHUB_PASSWORD &&
+update_remote_to_local_links &&
 iscc packaging/create_install_wizard.iss &&
 cd /home/Administrator/packaging/output/ && md5sum=$(md5sum -t *.exe) && echo $md5sum > ${md5sum##* }.md5 &&
 [ -z ${AWS_ACCESS_KEY} ] || upload_to_s3 "*.exe" && upload_to_s3 "*.md5"
