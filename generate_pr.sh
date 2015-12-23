754ed0d411307aef691f97415ba403ac5c03231f
fogelomer <omer@gigaspaces.com>
2015-12-23 11:15:06 +0200

    update circle.yml to deal wit PR

--- generate_pr.sh
+++ generate_pr.sh
@@ -4,7 +4,7 @@ echo $CI_PULL_REQUEST
 if [ -n "$CI_PULL_REQUEST" ]; then
 	git clone --depth=50 --branch=$CIRCLE_BRANCH  https://github.com/cloudify-cosmo/$CIRCLE_PROJECT_REPONAME.git cloudify-cosmo/$CIRCLE_PROJECT_REPONAME
         cd cloudify-cosmo/$CIRCLE_PROJECT_REPONAME
-        git fetch origin $CI_PULL_REQUEST/merge:
+        git fetch origin +refs/pull/254/merge:
 	git checkout -qf FETCH_HEAD
  	tox -e docs
         tox -e flake8
