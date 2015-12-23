f018e641bfaa1f26444117d03e9aaa9d3199b4f3
fogelomer <omer@gigaspaces.com>
2015-12-23 15:33:14 +0200

    update circle.yml to deal wit PR

--- generate_pr.sh
+++ generate_pr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+set -x
 
-echo $CI_PULL_REQUEST
 if [ -n "$CI_PULL_REQUEST" ]; then
 	PR_ID=${CI_PULL_REQUEST##*/}
 	git clone --depth=50 --branch=$CIRCLE_BRANCH  https://github.com/cloudify-cosmo/$CIRCLE_PROJECT_REPONAME.git cloudify-cosmo/$CIRCLE_PROJECT_REPONAME
