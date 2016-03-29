f4393206d061c8bb0193551d20aa50975b3c37bc
opencm <limor@gigaspaces.com>
2016-03-29 18:14:13 +0300

    Bump version

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -59,7 +59,7 @@ function build_rpm() {
 }
 
 
-CORE_TAG_NAME="3.4m3"
+CORE_TAG_NAME="3.4m4"
 curl https://raw.githubusercontent.com/cloudify-cosmo/cloudify-packager/$CORE_TAG_NAME/common/provision.sh -o ./common-provision.sh &&
 source common-provision.sh
 
