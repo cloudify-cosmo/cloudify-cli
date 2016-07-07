683a652733adb840c2100cbc2547fbed099cbe07
nir0s <nir36g@gmail.com>
2016-07-08 00:03:07 +0300

    x

--- cloudify_cli/bootstrap/bootstrap.py
+++ cloudify_cli/bootstrap/bootstrap.py
@@ -52,8 +52,8 @@ MANAGER_DEPLOYMENT_ARCHIVE_IGNORED_FILES = ['.git']
 MAX_MANAGER_DEPLOYMENT_SIZE = 50 * (10 ** 6)  # 50MB
 
 
-def _workdir(profile_name):
-    cloudify_dir = utils.get_init_path(profile_name)
+def _workdir():
+    cloudify_dir = utils.get_init_path()
     workdir = os.path.join(cloudify_dir, 'bootstrap')
     if not os.path.isdir(workdir):
         os.mkdir(workdir)
