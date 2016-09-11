a61f100cc8493d4cf119faab8c37ec40b53cd26f
limor-gs <limor@gigaspaces.com>
2016-09-11 17:53:34 +0300

    Update cloudify.rb

--- packaging/omnibus/config/projects/cloudify.rb
+++ packaging/omnibus/config/projects/cloudify.rb
@@ -11,7 +11,7 @@ homepage "http://getcloudify.org/"
 override :cacerts, version: '2015.10.28', source: { md5: '1fc922c991c9d8e7c4bfb3e2c64df3fa' }
 override :pip, version: '7.1.2', source: { md5: '3823d2343d9f3aaab21cf9c917710196' }
 override :setuptools, version: '18.5', source: { md5: '533c868f01169a3085177dffe5e768bb' }
-override :zlib, version: '1.2.8', source: { md5: '1142191120b845f4ed8c8c17455420ac' , url: 'http://repository.cloudifysource.org/org/cloudify3/components/zlib-1.2.8.tar.gz'}
+override :zlib, version: '1.2.8', source: { url: 'http://repository.cloudifysource.org/org/cloudify3/components/zlib-1.2.8.tar.gz'}
 
 # Defaults to C:/cloudify on Windows
 # and /opt/cfy on all other platforms
