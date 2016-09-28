0e075475fe7985f60ac2782dad41416c54e437a3
limor-gs <limor@gigaspaces.com>
2016-09-28 18:18:20 +0300

    Update cloudify.rb

--- packaging/omnibus/config/projects/cloudify.rb
+++ packaging/omnibus/config/projects/cloudify.rb
@@ -15,7 +15,7 @@ end
 maintainer "Gigaspaces"
 homepage "http://getcloudify.org/"
 
-override :cacerts, version: '2015.10.28', source: { md5: '782dcde8f5d53b1b9e888fdf113c42b9' }
+override :cacerts, version: '2015.10.28', source: { md5: '8d35a5cef6ce28da07867a0712558067' }
 override :pip, version: '7.1.2', source: { md5: '3823d2343d9f3aaab21cf9c917710196' }
 override :setuptools, version: '18.5', source: { md5: '533c868f01169a3085177dffe5e768bb' }
 override :zlib, version: '1.2.8', source: { md5: '44d667c142d7cda120332623eab69f40' , url: 'http://zlib.net/current/zlib-1.2.8.tar.gz'}
