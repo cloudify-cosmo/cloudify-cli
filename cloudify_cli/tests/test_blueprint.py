f9fad0e67de88c919d7fe3598096fb0fade05644
Javier Collado <javier@gigaspaces.com>
2016-10-13 17:35:36 +0200

    CFY-6006 Update blueprint test cases

--- cloudify_cli/tests/test_blueprint.py
+++ cloudify_cli/tests/test_blueprint.py
@@ -1,5 +1,6 @@
 import os
 
+from mock import patch
 from testtools import TestCase
 
 from .commands.constants import (
@@ -7,37 +8,58 @@ from .commands.constants import (
     SAMPLE_ARCHIVE_URL,
     SAMPLE_BLUEPRINT_PATH,
     SAMPLE_CUSTOM_NAME_ARCHIVE,
+    STUB_DIRECTORY_NAME,
 )
 from cloudify_cli import blueprint
 from cloudify_cli.exceptions import CloudifyCliError
 
 
-class TestGetBlueprint(TestCase):
+class TestGet(TestCase):
 
     """Test get a blueprint."""
 
     def test_yaml_path(self):
+        """Get a blueprint from a yaml file."""
         self.assertEqual(
             SAMPLE_BLUEPRINT_PATH,
             blueprint.get(SAMPLE_BLUEPRINT_PATH)
         )
 
-    def test_archive_default_name(self):
-        # Can't check the whole path here, as it's a randomly generated temp
-        self.assertIn(
-            'helloworld/blueprint.yaml',
-            blueprint.get(SAMPLE_ARCHIVE_PATH)
+    @patch('cloudify_cli.blueprint.os.path.isfile')
+    @patch('cloudify_cli.blueprint.os.listdir')
+    @patch('cloudify_cli.blueprint.utils.extract_archive')
+    def test_archive_default_name(self, extract_archive, listdir, isfile):
+        """Get a blueprint from a zip file."""
+        extract_archive.return_value = '/tmp'
+        listdir.return_value = ['directory']
+        isfile.return_value = True
+        self.assertEqual(
+            '/tmp/directory/blueprint.yaml',
+            blueprint.get(SAMPLE_ARCHIVE_PATH),
         )
 
-    def test_archive_custom_name(self):
-        # Can't check the whole path here, as it's a randomly generated temp
+    @patch('cloudify_cli.blueprint.os.path.isfile')
+    @patch('cloudify_cli.blueprint.os.listdir')
+    @patch('cloudify_cli.blueprint.utils.extract_archive')
+    def test_archive_custom_name(self, extract_archive, listdir, isfile):
+        """Get a blueprint with a custom name from a zip file."""
+        extract_archive.return_value = '/tmp'
+        listdir.return_value = ['directory']
+        isfile.return_value = True
         self.assertIn(
-            'helloworld/simple_blueprint.yaml',
+            'tmp/directory/simple_blueprint.yaml',
             blueprint.get(SAMPLE_CUSTOM_NAME_ARCHIVE, 'simple_blueprint.yaml')
         )
 
-    def test_archive_custom_name_no_default(self):
-        # There's no `blueprint.yaml` in the archive, so it should fail here
+    @patch('cloudify_cli.blueprint.os.path.isfile')
+    @patch('cloudify_cli.blueprint.os.listdir')
+    @patch('cloudify_cli.blueprint.utils.extract_archive')
+    def test_archive_custom_name_no_default(
+            self, extract_archive, listdir, isfile):
+        """Fail to get blueprint with a custom name from a zip file."""
+        extract_archive.return_value = '/tmp'
+        listdir.return_value = ['directory']
+        isfile.return_value = False
         self.assertRaises(
             CloudifyCliError,
             blueprint.get,
@@ -45,20 +67,21 @@ class TestGetBlueprint(TestCase):
         )
 
     def test_url_default_name(self):
-        # Can't check the whole path here, as it's a randomly generated temp
+        """Skip URL download."""
         self.assertEqual(
             blueprint.get(SAMPLE_ARCHIVE_URL),
             SAMPLE_ARCHIVE_URL,
         )
 
     def test_url_custom_name(self):
-        # Can't check the whole path here, as it's a randomly generated temp
+        """Ignore custom name in URL."""
         self.assertTrue(
             blueprint.get(SAMPLE_ARCHIVE_URL, 'ec2-blueprint.yaml'),
             SAMPLE_ARCHIVE_URL,
         )
 
     def test_bad_filename(self):
+        """Fail to get blueprint from a yaml file that doesn't exist."""
         self.assertRaises(
             CloudifyCliError,
             blueprint.get,
@@ -66,39 +89,50 @@ class TestGetBlueprint(TestCase):
         )
 
     def test_github_path(self):
+        """Map github repository path to URL."""
         # Can't check the whole path here, as it's a randomly generated temp
-        self.assertTrue(
-            blueprint.get(
-                'cloudify-cosmo/cloudify-hello-world-example'
-            ).endswith(
-                'cloudify-hello-world-example-master/blueprint.yaml',
-            )
+        self.assertEqual(
+            blueprint.get('cloudify-cosmo/cloudify-hello-world-example'),
+            (
+                'https://github.com/cloudify-cosmo/'
+                'cloudify-hello-world-example/archive/master.tar.gz'
+            ),
         )
 
     def test_github_path_custom_name(self):
-        # Can't check the whole path here, as it's a randomly generated temp
-        self.assertTrue(
+        """Map github repository path to URL and ignore custom name."""
+        self.assertEqual(
             blueprint.get(
                 'cloudify-cosmo/cloudify-hello-world-example',
                 'ec2-blueprint.yaml'
-            ).endswith(
-                'cloudify-hello-world-example-master/ec2-blueprint.yaml',
-            )
+            ),
+            (
+                'https://github.com/cloudify-cosmo/'
+                'cloudify-hello-world-example/archive/master.tar.gz'
+            ),
         )
 
+
+class TestGenerateId(TestCase):
+
+    """Test generate blueprint id."""
+
     def test_generate_id_default(self):
+        """Generate blueprint id from directory."""
         self.assertEqual(
-            'helloworld',
-            blueprint.generate_id(SAMPLE_BLUEPRINT_PATH)
+            STUB_DIRECTORY_NAME,
+            blueprint.generate_id(SAMPLE_BLUEPRINT_PATH),
         )
 
     def test_generate_id_custom(self):
+        """Generate blueprint id from directory and custom filename."""
         self.assertEqual(
-            'helloworld.test',
-            blueprint.generate_id(SAMPLE_BLUEPRINT_PATH, 'test')
+            '{0}.test'.format(STUB_DIRECTORY_NAME),
+            blueprint.generate_id(SAMPLE_BLUEPRINT_PATH, 'test.yaml')
         )
 
     def test_generate_id_in_blueprint_folder(self):
+        """Generate blueprint id from relative directory."""
         self.assertEqual(
             'helloworld',
             blueprint.generate_id(os.path.join('.', SAMPLE_BLUEPRINT_PATH)))
