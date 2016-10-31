094dc2b75b8554e61a13ce288821efee6a3f3b0d
asdf <tehasdf@gmail.com>
2016-10-31 08:40:15 +0100

    CFY-6009 Add the 'cfy ha start' command for starting a HA cluster
    This commands starts a HA cluster on the current manager, and sets
    it as the leader of the cluster.

--- cloudify_cli/commands/cluster.py
+++ cloudify_cli/commands/cluster.py
@@ -14,12 +14,18 @@
 # limitations under the License.
 ############
 
+import time
+from datetime import datetime
+
 from .. import env
 from ..cli import cfy
 from ..exceptions import CloudifyCliError
+from ..execution_events_fetcher import WAIT_FOR_EXECUTION_SLEEP_INTERVAL
 
 
 def _verify_not_in_cluster(client):
+    """Check that the current manager doesn't already belong to a cluster
+    """
     status = client.cluster.status()
     if status.initialized:
         raise CloudifyCliError('This manager machine is already part '
@@ -48,3 +54,82 @@ def status(client, logger):
     else:
         logger.info('HA cluster initialized!\nEncryption key: {0}'
                     .format(status.encryption_key))
+
+
+@cluster.command(name='start',
+                 short_help='Start a HA manager cluster [manager only]')
+@cfy.pass_client()
+@cfy.pass_logger
+@cfy.options.timeout()
+@cfy.options.cluster_host_ip
+@cfy.options.cluster_node_name
+@cfy.options.cluster_encryption_key(with_default=True)
+def start(client,
+          logger,
+          timeout,
+          cluster_host_ip,
+          cluster_node_name,
+          cluster_encryption_key):
+    """Start a HA manager cluster with the current manager as the master.
+
+    This will initialize all the HA cluster components on the current manager,
+    and mark it as the master. After that, other managers will be able to
+    join the cluster by passing this manager's IP address and encryption key.
+    """
+
+    _verify_not_in_cluster(client)
+
+    logger.info('Creating a new Cloudify HA cluster')
+
+    client.cluster.start(config={
+        'host_ip': cluster_host_ip,
+        'node_name': cluster_node_name,
+        'encryption_key': cluster_encryption_key
+    })
+    status = _wait_for_cluster_initialized(client, logger)
+
+    if status.error:
+        logger.error('Error while configuring the HA cluster')
+        raise CloudifyCliError(status.error)
+
+    logger.info('HA cluster started at {0}.\n'
+                'Encryption key used is: {1}'
+                .format(cluster_host_ip, cluster_encryption_key))
+
+
+def _wait_for_cluster_initialized(client, logger=None, timeout=900):
+    # this is similar to how an execution's logs and status are fetched,
+    # but is using a different backend mechanism
+    include = None
+    if logger is not None:
+        # only fetch logs when the logger is present - no need otherwise
+        include = ['logs', 'initialized', 'error']
+
+    # each log message will have a "cursor" value - we need to tell the server
+    # what's the latest cursor position we've already seen, so that it'll only
+    # yield logs more recent than that.
+    last_log = None
+
+    while True:
+        if timeout is not None:
+            timeout -= WAIT_FOR_EXECUTION_SLEEP_INTERVAL
+            if timeout < 0:
+                raise CloudifyCliError('Timed out waiting for the HA '
+                                       'cluster to be initialized.')
+
+        status = client.cluster.status(
+            _include=include,
+            since=last_log)
+        if status.logs:
+            last_log = status.logs[-1]['cursor']
+
+        for log in status.logs:
+            timestamp = datetime.utcfromtimestamp(log['timestamp'] // 1e6)
+            logger.info('{0} {1}'.format(timestamp.isoformat(),
+                                         log['message']))
+
+        if status.initialized or status.error:
+            break
+        time.sleep(WAIT_FOR_EXECUTION_SLEEP_INTERVAL)
+
+    return status
