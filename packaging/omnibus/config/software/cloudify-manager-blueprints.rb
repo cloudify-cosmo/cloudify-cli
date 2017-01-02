93ddf0735d8b582e136620db19536eacfc5a7a1a
limor-gs <limor@gigaspaces.com>
2017-01-02 16:31:17 +0200

    new versioning

--- packaging/omnibus/config/software/cloudify-manager-blueprints.rb
+++ packaging/omnibus/config/software/cloudify-manager-blueprints.rb
@@ -2,7 +2,7 @@ name "cloudify-manager-blueprints"
 
 ENV['CORE_TAG_NAME'] || raise('CORE_TAG_NAME environment variable not set')
 default_version ENV['CORE_TAG_NAME']
-manager_single_tar_url ENV['SINGLE_TAR_URL']
+manager_single_tar_url=ENV['SINGLE_TAR_URL']
 
 source :git => "https://github.com/cloudify-cosmo/cloudify-manager-blueprints"
 
