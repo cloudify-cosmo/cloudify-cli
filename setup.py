a6e3eaa4f77a0b45096dd98d126902af98729d20
opencm <limor@gigaspaces.com>
2017-01-25 15:31:25 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify',
-    version='4.0a12',
+    version='4.0a13',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['cloudify_cli',
@@ -52,10 +52,10 @@ setup(
         'requests==2.7.0',
         'PrettyTable>=0.7,<0.8',
         'click_didyoumean==0.0.3',
-        'cloudify-dsl-parser==4.0a12',
+        'cloudify-dsl-parser==4.0a13',
         'cloudify-script-plugin==1.4',
-        'cloudify-rest-client==4.0a12',
-        'cloudify-plugins-common==4.0a12',
+        'cloudify-rest-client==4.0a13',
+        'cloudify-plugins-common==4.0a13',
         'backports.shutil_get_terminal_size==1.0.0',
     ]
 )
