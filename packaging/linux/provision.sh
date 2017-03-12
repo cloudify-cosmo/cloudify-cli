d542ee483e63f0f276050c8d874d4ffb8a55959c
opencm <limor@gigaspaces.com>
2017-03-12 17:00:22 +0200

    Bump version

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -7,7 +7,7 @@ AWS_ACCESS_KEY=$4
 CLI_BRANCH=$5
 PACKAGER_BRANCH=$6
 export REPO=$7
-export CORE_TAG_NAME="4.0m15"
+export CORE_TAG_NAME="4.0rc1"
 
 if [ $REPO == "cloudify-versions" ];then
     REPO_TAG="master"
