53bb3c585c1ec0429e033adaca2ba7c858086fd8
opencm <opencm@gigaspaces.com>
2017-07-02 13:40:52 +0300

    Update cloudify.rb

--- packaging/omnibus/config/projects/cloudify.rb
+++ packaging/omnibus/config/projects/cloudify.rb
@@ -8,6 +8,8 @@ name "cloudify"
 maintainer "Gigaspaces"
 homepage "http://getcloudify.org/"
 
+ENV['CORE_TAG_NAME'] || raise('CORE_TAG_NAME environment variable not set')
+
 override :cacerts, version: '2015.10.28', source: { md5: '9d8c2e9a93881cdf1f2a7fc3d01a6318' }
 override :pip, version: '7.1.2', source: { md5: '3823d2343d9f3aaab21cf9c917710196' }
 override :setuptools, version: '18.5', source: { md5: '533c868f01169a3085177dffe5e768bb' }
@@ -17,7 +19,7 @@ override :zlib, version: '1.2.8', source: { md5: '44d667c142d7cda120332623eab69f
 # and /opt/cfy on all other platforms
 install_dir "#{default_root}/cfy"
 
-build_version Omnibus::BuildVersion
+build_version ENV['CORE_TAG_NAME']
 
 # Creates required build directories
 dependency "preparation"
