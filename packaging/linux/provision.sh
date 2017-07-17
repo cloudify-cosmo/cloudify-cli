f336e48e07e69d4f0d7dd97940e66dee5fbf7bf0
limor-gs <limor@gigaspaces.com>
2017-07-17 17:11:56 +0300

    Update provision.sh

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -23,11 +23,11 @@ rm -rf cloudify-cli
 git clone https://github.com/cloudify-cosmo/cloudify-cli.git
 cd cloudify-cli/packaging/omnibus
 gitTagExists=$(git tag -l $CORE_TAG_NAME)
-#if [ "$CORE_BRANCH" != "master" ]; then
-    git checkout -b ${CORE_BRANCH} origin/remove-cloud-plugins
-#else
-#    git checkout ${CORE_BRANCH}
-#fi
+if [ "$CORE_BRANCH" != "master" ]; then
+    git checkout -b ${CORE_BRANCH} origin/${CORE_BRANCH}
+else
+    git checkout ${CORE_BRANCH}
+fi
 omnibus build cloudify && result="success"
 cd pkg
 cat *.json || exit 1
