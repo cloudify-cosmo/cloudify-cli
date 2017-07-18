80b83efaacfda9022dc0c5a7854391fdf1e4ca78
limor-gs <limor@gigaspaces.com>
2017-07-18 12:03:15 +0300

    Update cloudify-manager-blueprints.rb

--- packaging/omnibus/config/software/cloudify-manager-blueprints.rb
+++ packaging/omnibus/config/software/cloudify-manager-blueprints.rb
@@ -9,7 +9,8 @@ source :git => "https://github.com/cloudify-cosmo/cloudify-manager-blueprints"
 build do
    command "cp -r ../cloudify-manager-blueprints /opt/cfy/"
 
-   str_to_replace="s|default:.*cloudify-manager-resources.*|default: #{manager_single_tar_url}|g"
+   str='!b;'
+   str_to_replace="/manager_resources_package:/${str}n;n;n;c\ \ \ \ default:  #{manager_single_tar_url}"
    cmd="sed -i \"#{str_to_replace}\" /opt/cfy/cloudify-manager-blueprints/inputs/manager-inputs.yaml"
    command cmd
    str_to_replace="s|.*#manager_resources_package:.*|#manager_resources_package: #{manager_single_tar_url}|g"
