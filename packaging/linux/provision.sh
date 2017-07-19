f2bcc6e1c80649db41fb5e82e7b1b277e31d803c
limor-gs <limor@gigaspaces.com>
2017-07-19 16:20:14 +0300

    new build system

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -8,7 +8,7 @@ CLI_BRANCH=$5
 PACKAGER_BRANCH=$6
 export REPO=$7
 export CORE_TAG_NAME="4.1.1"
-export CORE_BRANCH="master"
+export CORE_BRANCH="4.1.1-build"
 
 curl -u $GITHUB_USERNAME:$GITHUB_PASSWORD https://raw.githubusercontent.com/cloudify-cosmo/${REPO}/${CORE_BRANCH}/packages-urls/common_build_env.sh -o ./common_build_env.sh &&
 source common_build_env.sh &&
