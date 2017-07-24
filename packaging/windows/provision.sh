69b38042ec12da772f42eac0b67dade34faced66
Yariv Hamamy <yariv@gigaspaces.com>
2017-07-24 13:45:25 +0300

    CFY-7078 tmp

--- packaging/windows/provision.sh
+++ packaging/windows/provision.sh
@@ -12,17 +12,17 @@ function download_wheels() {
     curl -LO https://pypi.python.org/packages/2.7/l/lxml/lxml-3.5.0.win32-py2.7.exe
     wheel convert lxml-3.5.0.win32-py2.7.exe --dest-dir packaging/source/wheels
 
-    PATCH_URL="https://raw.githubusercontent.com/cloudify-cosmo/cloudify-cli/${CORE_BRANCH}/packaging/omnibus/config/patches/cloudify-cli/cloudify_cli.patch"
-    curl -sLO https://github.com/cloudify-cosmo/cloudify-cli/archive/${CORE_BRANCH}.zip
-    unzip -q -o ${CORE_BRANCH}.zip
-    [[ -f ${CORE_BRANCH}.zip ]] && rm -f ${CORE_BRANCH}.zip
-    curl -sL "${PATCH_URL}" -o cloudify-cli-${CORE_BRANCH}/cloudify_cli.patch
-    patch -p1 -d cloudify-cli-${CORE_BRANCH} < cloudify-cli-${CORE_BRANCH}/cloudify_cli.patch
-    rm -f cloudify-cli-${CORE_BRANCH}/cloudify_cli.patch
-    zip -q -r cloudify-cli-${CORE_BRANCH}.zip cloudify-cli-${CORE_BRANCH}
-    [[ $? -eq 0 ]] && rm -rf cloudify-cli-${CORE_BRANCH}
+    PATCH_URL="https://raw.githubusercontent.com/cloudify-cosmo/cloudify-cli/CFY-7078-windows-cli-packages-are-broken/packaging/omnibus/config/patches/cloudify-cli/cloudify_cli.patch"
+    curl -sLO https://github.com/cloudify-cosmo/cloudify-cli/archive/CFY-7078-windows-cli-packages-are-broken.zip
+    unzip -q -o CFY-7078-windows-cli-packages-are-broken.zip
+    [[ -f CFY-7078-windows-cli-packages-are-broken.zip ]] && rm -f CFY-7078-windows-cli-packages-are-broken.zip
+    curl -sL "${PATCH_URL}" -o cloudify-cli-CFY-7078-windows-cli-packages-are-broken/cloudify_cli.patch
+    patch -p1 -d cloudify-cli-CFY-7078-windows-cli-packages-are-broken < cloudify-cli-CFY-7078-windows-cli-packages-are-broken/cloudify_cli.patch
+    rm -f cloudify-cli-CFY-7078-windows-cli-packages-are-broken/cloudify_cli.patch
+    zip -q -r cloudify-cli-CFY-7078-windows-cli-packages-are-broken.zip cloudify-cli-CFY-7078-windows-cli-packages-are-broken
+    [[ $? -eq 0 ]] && rm -rf cloudify-cli-CFY-7078-windows-cli-packages-are-broken
 
-    pip wheel --wheel-dir packaging/source/wheels --find-links packaging/source/wheels C:/Cygwin/home/Administrator/cloudify-cli-${CORE_BRANCH}.zip \
+    pip wheel --wheel-dir packaging/source/wheels --find-links packaging/source/wheels C:/Cygwin/home/Administrator/cloudify-cli-CFY-7078-windows-cli-packages-are-broken.zip \
     https://github.com/cloudify-cosmo/cloudify-rest-client/archive/${CORE_BRANCH}.zip#egg=cloudify-rest-client \
     https://github.com/cloudify-cosmo/cloudify-dsl-parser/archive/${CORE_BRANCH}.zip#egg=cloudify-dsl-parser \
     https://github.com/cloudify-cosmo/cloudify-plugins-common/archive/${CORE_BRANCH}.zip#egg=cloudify-plugins-common \
