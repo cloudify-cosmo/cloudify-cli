9e6187174033d70c034f74590aee1eadf640ff14
Assi Maimon <assi@gigaspaces.com>
2017-08-06 11:59:00 +0300

    add-osx-support

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -10,28 +10,93 @@ export REPO=$7
 export CORE_TAG_NAME="4.2.dev1"
 export CORE_BRANCH="master"
 
-curl -u $GITHUB_USERNAME:$GITHUB_PASSWORD https://raw.githubusercontent.com/cloudify-cosmo/${REPO}/${CORE_BRANCH}/packages-urls/common_build_env.sh -o ./common_build_env.sh &&
-source common_build_env.sh &&
 curl https://raw.githubusercontent.com/cloudify-cosmo/cloudify-packager/${CORE_BRANCH}/common/provision.sh -o ./common-provision.sh &&
 source common-provision.sh
+curl -u $GITHUB_USERNAME:$GITHUB_PASSWORD https://raw.githubusercontent.com/cloudify-cosmo/${REPO}/${CORE_BRANCH}/packages-urls/common_build_env.sh -o ./common_build_env.sh &&
+source common_build_env.sh &&
 curl -u $GITHUB_USERNAME:$GITHUB_PASSWORD https://raw.githubusercontent.com/cloudify-cosmo/${REPO}/${CORE_BRANCH}/packages-urls/manager-single-tar.yaml -o ./manager-single-tar.yaml &&
 export SINGLE_TAR_URL=$(cat manager-single-tar.yaml)
 
 
 install_common_prereqs &&
 rm -rf cloudify-cli
-git clone https://github.com/cloudify-cosmo/cloudify-cli.git
-cd cloudify-cli/packaging/omnibus
-gitTagExists=$(git tag -l $CORE_TAG_NAME)
-if [ "$CORE_BRANCH" != "master" ]; then
-    git checkout -b ${CORE_BRANCH} origin/${CORE_BRANCH}
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    git clone -b add-osx-support https://github.com/cloudify-cosmo/cloudify-cli.git
 else
-    git checkout ${CORE_BRANCH}
+    git clone https://github.com/cloudify-cosmo/cloudify-cli.git
 fi
+cd cloudify-cli/packaging/omnibus
+#gitTagExists=$(git tag -l $CORE_TAG_NAME)
+#if [ "$CORE_BRANCH" != "master" ]; then
+#    git checkout -b ${CORE_BRANCH} origin/${CORE_BRANCH}
+#else
+#    git checkout ${CORE_BRANCH}
+#fi
+
+# OSX preparation
+function prepare_osx () {
+    which -s brew
+    if [[ $? != 0 ]] ; then
+        echo "Missing Brew isntaller - Make sure your system has Brew"
+        echo "Install Brew with sudo and run agian"
+    else
+        echo "Brew already installed. Updating"
+        brew update
+    fi
+    brew install rbenv ruby-build
+
+    if [[ 'grep "if which rbenv" ~/.bash_profile' != 0 ]]; then
+        source ~/.bash_profile
+    else
+        echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
+        source ~/.bash_profile
+    fi
+
+    if [[ $(rbenv version | cut -d' ' -f1) != '2.2.1' ]] ; then
+        echo "Installing rbenv version 2.2.1"
+        rbenv install 2.2.1 -s
+    else
+        echo "rbenv 2.2.1 is installed"
+    fi
+    rbenv global 2.2.1
+    if [[ $(gem list |grep bundler) != 'bundler (1.8.4)' ]] ; then
+        gem install bundler -v '=1.8.4' --no-ri --no-rdoc
+    fi
+    which -s omnibus
+    if [[ $? != 0 ]] ; then
+        gem install omnibus --no-ri --no-rdoc
+    fi
+}
+
+# OSX manipulation
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    prepare_osx
+    mkdir omnibus_source
+    git clone https://github.com/chef/omnibus-software.git --depth 1
+    list_of_omnibus_softwares="gdbm cacerts config_guess gdbm libffi makedepend
+     ncurses openssl pkg-config-lite setuptools
+     util-macros version-manifest xproto zlib"
+    for omnibus_softwate in $list_of_omnibus_softwares
+    do
+       if [[ -e omnibus-software/config/software/$omnibus_softwate.rb ]] ; then
+         cp -r omnibus-software/config/software/$omnibus_softwate.rb \
+         config/software/$omnibus_softwate.rb
+       else
+         echo "Missing software in Omnibus-Software repo"
+         exit
+       fi
+       [[ -e omnibus-software/config/patches/$omnibus_softwate ]] &&
+       cp -r omnibus-software/config/patches/$omnibus_softwate config/patches/
+    done
+    cp -r omnibus-software/config/templates/* config/templates/
+    grep -l '/opt' config/software/* | xargs sed -i "" 's|/opt|/usr/local/opt|g'
+fi
+
 omnibus build cloudify && result="success"
 cd pkg
 cat *.json || exit 1
 rm -f version-manifest.json
+[ $(ls | grep pkg | sed -n 2p ) ] && FILEEXT="pkg"
 [ $(ls | grep rpm | sed -n 2p ) ] && FILEEXT="rpm"
 [ $(ls | grep deb | sed -n 2p ) ] && FILEEXT="deb"
 
