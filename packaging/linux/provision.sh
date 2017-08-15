ae7e02bad7eab588ce1b4c96770264bd509798ed
opencm <opencm@gigaspaces.com>
2017-08-15 10:46:22 +0300

    Update provision.sh

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -28,7 +28,8 @@ git clone https://github.com/cloudify-cosmo/cloudify-cli.git
 cd cloudify-cli/packaging/omnibus
 gitTagExists=$(git tag -l $CORE_TAG_NAME)
 if [ "$CORE_BRANCH" != "master" ]; then
-    git checkout -b ${CORE_BRANCH} origin/${CORE_BRANCH}
+    #git checkout -b ${CORE_BRANCH} origin/${CORE_BRANCH}
+    git checkout -b add-osx-support origin/add-osx-support
 else
     git checkout ${CORE_BRANCH}
 fi
