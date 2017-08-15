d38211169cd021dbd47871a5d3f5d70701197d55
opencm <opencm@gigaspaces.com>
2017-08-15 14:54:43 +0300

    Update provision.sh

--- packaging/windows/provision.sh
+++ packaging/windows/provision.sh
@@ -119,7 +119,6 @@ if [ $REPO == "cloudify-versions" ];then
 fi
 export SINGLE_TAR_URL=$(cat manager-single-tar.yaml)
 
-install_common_prereqs &&
 install_requirements &&
 download_wheels $GITHUB_USERNAME $GITHUB_PASSWORD &&
 download_resources $GITHUB_USERNAME $GITHUB_PASSWORD &&
