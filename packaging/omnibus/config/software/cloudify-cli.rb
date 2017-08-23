8193840a4fdba807b1a1d6fc1a5e8f0e40ba7917
limor-gs <limor@gigaspaces.com>
2017-08-23 11:58:41 +0300

    clap

--- packaging/omnibus/config/software/cloudify-cli.rb
+++ packaging/omnibus/config/software/cloudify-cli.rb
@@ -28,33 +28,31 @@ github_username=ENV['GITHUB_USERNAME']
 github_password=ENV['GITHUB_PASSWORD']
 
 dependency "python"
-if ! windows?
+
   dependency "pip"
-end
+
 
 source git: "https://github.com/cloudify-cosmo/cloudify-cli"
 
 build do
 
-  command "curl https://raw.githubusercontent.com/cloudify-cosmo/cloudify-dev/install--upgrade/scripts/clap -o ./clap && chmod +x ./clap"
-  command "pip install sh argh colorama"
-  command "export CLAP_REPO_BASE=./dev/repos"
-  command "export REPOS_BASE_GITHUB_URL=https://github.com/cloudify-cosmo/{0}.git"
-  cmd="./clap setup -r ./build-requirements.txt -b #{branch} -d"
-  command "git reset --hard HEAD"
+    command "curl https://raw.githubusercontent.com/cloudify-cosmo/cloudify-dev/install--upgrade/scripts/clap -o ./clap && chmod +x ./clap"
+    command "sudo pip install virtualenv"
+    command "virtualenv env && source env"
+    command "pip install sh argh colorama"
+    command "export CLAP_REPO_BASE=./dev/repos"
+    command "export REPOS_BASE_GITHUB_URL=https://github.com/cloudify-cosmo/{0}.git"
+    command "git reset --hard HEAD"
 
-  if windows?
-    
-    command "#{cmd}"
-  else
     # previous patch gets cached
     patch source: "cloudify_cli.patch"
 
-    command "#{cmd}"
+    command ["#{install_dir}/embedded/bin/virtualenv", "env"]
+    command "source env"
+    command "./clap setup -r ./build-requirements.txt -b #{branch} -d"
 
     erb :dest => "#{install_dir}/bin/cfy",
       :source => "cfy_wrapper.erb",
       :mode => 0755,
       :vars => { :install_dir => install_dir }
-  end
 end
