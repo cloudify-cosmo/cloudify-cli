db862ad4f83e1d306276986867adfe0ff3e7129a
limor-gs <limor@gigaspaces.com>
2017-09-28 11:14:54 +0300

    Removing zlib 1.2.8 md5 (#680)

--- packaging/omnibus/config/projects/cloudify.rb
+++ packaging/omnibus/config/projects/cloudify.rb
@@ -16,7 +16,7 @@ cloudify_pre=ENV['PRERELEASE']
 override :cacerts, version: '2017-06-07', source: { sha256: '435ac8e816f5c10eaaf228d618445811c16a5e842e461cb087642b6265a36856' }
 override :pip, version: '7.1.2', source: { md5: '3823d2343d9f3aaab21cf9c917710196' }
 override :setuptools, version: '18.5', source: { md5: '533c868f01169a3085177dffe5e768bb' }
-override :zlib, version: '1.2.8', source: { md5: 'cb530d737c8f2d1023797cf0587b4e05'}
+override :zlib, version: '1.2.8'
 
 # Defaults to C:/cloudify on Windows
 # and /opt/cfy on all other platforms
