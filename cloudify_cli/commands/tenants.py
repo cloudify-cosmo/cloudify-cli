ca7d1fb99a778d5f2df638904ee0f50a2ff38e77
Javier Collado <javier@gigaspaces.com>
2017-10-20 12:55:46 +0200

    CFY-7373 Move group_role option to a static method
    This is to add the required parameter

--- cloudify_cli/commands/tenants.py
+++ cloudify_cli/commands/tenants.py
@@ -142,7 +142,7 @@ def remove_user(username, tenant_name, logger, client):
 @tenants.command(name='add-user-group',
                  short_help='Add a user group to a tenant [manager only]')
 @cfy.argument('user-group-name', callback=cfy.validate_name)
-@cfy.options.group_role
+@cfy.options.group_role()
 @cfy.options.tenant_name(show_default_in_help=False)
 @cfy.options.verbose()
 @cfy.assert_manager_active()
