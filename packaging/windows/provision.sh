18b38faa9c2690f6537569f62ed97c910b706c07
opencm <opencm@gigaspaces.com>
2017-12-18 14:20:21 +0200

    Update provision.sh

--- packaging/windows/provision.sh
+++ packaging/windows/provision.sh
@@ -23,7 +23,7 @@ function download_wheels() {
     [[ $? -eq 0 ]] && rm -rf cloudify-cli-${CORE_BRANCH}
 
     pip wheel --wheel-dir packaging/source/wheels --find-links packaging/source/wheels C:/Cygwin/home/Administrator/cloudify-cli-${CORE_BRANCH}.zip \
-    https://github.com/cloudify-cosmo/cloudify-rest-client/archive/${CORE_BRANCH}.zip#egg=cloudify-rest-client \
+    https://github.com/cloudify-cosmo/cloudify-rest-client/archive/CFY-7687-Remove-aria-dependency-from-rest-client.zip#egg=cloudify-rest-client \
     https://github.com/cloudify-cosmo/cloudify-dsl-parser/archive/${CORE_BRANCH}.zip#egg=cloudify-dsl-parser \
     https://github.com/cloudify-cosmo/cloudify-plugins-common/archive/${CORE_BRANCH}.zip#egg=cloudify-plugins-common \
     https://github.com/cloudify-cosmo/cloudify-script-plugin/archive/1.5.1.zip#egg=cloudify-script-plugin \
