3e34f8b4c3a67d95d0c1fef4959792cad3b3205a
opencm <opencm@gigaspaces.com>
2018-03-20 16:52:23 +0200

    Update provision.sh

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -4,8 +4,8 @@ export GITHUB_USERNAME=$1
 export GITHUB_PASSWORD=$2
 export AWS_ACCESS_KEY_ID=$3
 export AWS_ACCESS_KEY=$4
-export BRANCH=$5
-export REPO=$6
+export REPO=$5
+export BRANCH=$6
 export CORE_TAG_NAME="4.4.dev1"
 export CORE_BRANCH="master"
 
