a7373aba5b9a884c88425536454aaa77bb9dba07
David Ginzbourg <davidginzbourg@gmail.com>
2018-09-28 15:17:46 -0700

    Add jaeger codecs

--- cloudify_cli/tracer.py
+++ cloudify_cli/tracer.py
@@ -1,14 +1,18 @@
 import opentracing
+from jaeger_client import constants, codecs
 from opentracing_instrumentation.request_context import get_current_span, \
     span_in_context
 
 from logger import get_logger
 
 logger = get_logger()
+
 _tracer = None
 
 
 class Tracer(object):
+    """Simplified tracer class.
+    """
     def __init__(self, operation_name):
         curr_span = get_current_span()
         self.span = opentracing.Tracer().start_span(
@@ -17,10 +21,33 @@ class Tracer(object):
         self.span.__enter__()
         self.span_ctx = span_in_context(self.span)
         self.span_ctx.__enter__()
+        self._tracer_http_codec = self._get_http_codec()
 
-    def destroy(self, exc_type, exc_val, exc_tb):
-        self.span_ctx.__exit__(exc_type, exc_val, exc_tb)
-        self.span.__exit__(exc_type, exc_val, exc_tb)
+    def destroy(self):
+        """Destroys the span and span context.
+        """
+        self.span_ctx.__exit__(None, None, None)
+        self.span.__exit__(None, None, None)
+
+    @staticmethod
+    def _get_http_codec():
+        """Initializes an HTTP codec to serialize the span context.
+
+        :return: an initialized HTTP codec instance.
+        """
+        return codecs.TextCodec(
+            url_encoding=True,
+            trace_id_header=constants.TRACE_ID_HEADER,
+            baggage_header_prefix=constants.BAGGAGE_HEADER_PREFIX,
+            debug_id_header=constants.DEBUG_ID_HEADER_KEY)
+
+    def inject(self, span_context, carrier):
+        """Serializes the span_context and adds it to the carrier.
+
+        :param span_context: span context to serialize.
+        :param carrier: a dict obj to add the serialized span context to.
+        """
+        self._tracer_http_codec.inject(span_context, carrier)
 
 
 def init_tracing(operation_name):
