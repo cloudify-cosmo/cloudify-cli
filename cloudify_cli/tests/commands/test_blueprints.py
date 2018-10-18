d78c8abe05fdfa6010825c727fb2cc88094454f7
tyacbovi <tyacbovi@gmail.com>
2018-10-18 13:28:52 +0300

    CY-693: adding blueprint force deletion

--- cloudify_cli/tests/commands/test_blueprints.py
+++ cloudify_cli/tests/commands/test_blueprints.py
@@ -1,3 +1,19 @@
+########
+# Copyright (c) 2018 Cloudify Platform Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+############
+
 import os
 import json
 import yaml
@@ -63,6 +79,14 @@ class BlueprintsTest(CliCommandTest):
     def test_blueprints_delete(self):
         self.client.blueprints.delete = MagicMock()
         self.invoke('blueprints delete a-blueprint-id')
+
+    def test_blueprints_delete_force(self):
+        for flag in ['--force', '-f']:
+            mock = Mock()
+            self.client.blueprints.delete = mock
+            self.invoke('blueprints delete a-blueprint-id {0}'.format(
+                flag))
+            mock.assert_called_once_with('a-blueprint-id', True)
 
     def test_blueprints_delete_explicit_tenant(self):
         self.client.blueprints.delete = MagicMock()
