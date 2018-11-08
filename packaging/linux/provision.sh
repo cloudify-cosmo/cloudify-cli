287fd33fc55f1a9d7cd6ec501f3664b450ed48a9
opencm <limor@gigaspaces.com>
2018-11-08 10:25:27 +0200

    Bump version

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -6,8 +6,8 @@ AWS_ACCESS_KEY_ID=$3
 AWS_ACCESS_KEY=$4
 PACKAGER_BRANCH=$5
 export REPO=$6
-export CORE_TAG_NAME="4.3.3"
-export CORE_BRANCH="4.3.3-build"
+export CORE_TAG_NAME="4.3.4"
+export CORE_BRANCH="4.3.4-build"
 
 
 # OSX preparation
--- packaging/windows/provision.sh
+++ packaging/windows/provision.sh
@@ -94,8 +94,8 @@ GITHUB_PASSWORD=$2
 AWS_ACCESS_KEY_ID=$3
 AWS_ACCESS_KEY=$4
 export REPO=$5
-export CORE_TAG_NAME="4.3.3"
-export CORE_BRANCH="4.3.3-build"
+export CORE_TAG_NAME="4.3.4"
+export CORE_BRANCH="4.3.4-build"
 
 curl -u $GITHUB_USERNAME:$GITHUB_PASSWORD https://raw.githubusercontent.com/cloudify-cosmo/${REPO}/${CORE_BRANCH}/packages-urls/common_build_env.sh -o ./common_build_env.sh &&
 source common_build_env.sh &&
