cdaace0805a41e1676ecac954c5953ca981c1887
Dan Alima <ddalima@gmail.com>
2018-12-16 15:33:14 +0200

    add signature for rvm

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -58,6 +58,7 @@ function prepare_linux () {
         gpg=gpg
     fi
 
+    curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
     $gpg --keyserver hkp://keys.gnupg.net --recv-keys \
         409B6B1796C275462A1703113804BB82D39DC0E3 \
         7D2BAF1CF37B13E2069D6956105BD0E739499BDB
