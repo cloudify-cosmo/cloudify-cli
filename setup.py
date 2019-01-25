c300d6d9e711804a093a5d78902f94cdbfd67f69
opencm <limor@gigaspaces.com>
2019-01-25 03:27:31 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify',
-    version='4.5.5',
+    version='19.01.24',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['cloudify_cli',
