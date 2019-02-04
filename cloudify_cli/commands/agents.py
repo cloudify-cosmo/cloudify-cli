477e10b7e28f7199dfd9df317d737048c363b478
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2019-02-04 19:32:15 +0100

    Agents install: allow_custom_parameters=True
    This allows using --stop-old-agents on deployments using an old
    types.yaml

--- cloudify_cli/commands/agents.py
+++ cloudify_cli/commands/agents.py
@@ -240,7 +240,8 @@ def run_worker(
         timeout = 900
         try:
             execution = client.executions.start(
-                dep_id, workflow_id, parameters)
+                dep_id, workflow_id, parameters,
+                allow_custom_parameters=True)
             execution = wait_for_execution(
                 client,
                 execution,
