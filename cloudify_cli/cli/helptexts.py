5eff3f06f00d95da01de02aaade05dd56164c052
asdf <tehasdf@gmail.com>
2019-02-07 02:23:32 +0100

    Change preview helptext

--- cloudify_cli/cli/helptexts.py
+++ cloudify_cli/cli/helptexts.py
@@ -89,10 +89,10 @@ INSTALL_FIRST = (
     "workflow. default: uninstall and then install"
 )
 PREVIEW = (
-    "In deployment update, perform a preview of an update and not an actual "
-    "update. The changes that should be done will be returned, but no change "
-    "will actually take effect"
+    "Preview the deployment update, stating what changes will be made "
+    "without actually applying any changes."
 )
+
 FORCE_UPDATE = (
     "Force running update in case a previous update on this deployment has "
     "failed to finished successfully"
