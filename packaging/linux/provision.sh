740833ceb02d516b21dba04e1f3ac34058b70d9b
dan-cloudify <43587515+dan-cloudify@users.noreply.github.com>
2019-03-18 18:30:40 +0200

    update gpg server url (#925)

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -59,7 +59,7 @@ function prepare_linux () {
     fi
 
     curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
-    $gpg --keyserver hkp://keys.gnupg.net --recv-keys \
+    $gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys \
         409B6B1796C275462A1703113804BB82D39DC0E3 \
         7D2BAF1CF37B13E2069D6956105BD0E739499BDB
     curl -sSL https://get.rvm.io | bash -s stable
