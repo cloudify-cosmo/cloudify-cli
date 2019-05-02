8e2924dd20d03354814fd04014b9b40efc640628
Inbal Amrani <inbala@gigaspaces.com>
2019-05-02 17:44:45 +0300

    CY-1246 Change the format of the site's location (#935)
    * Change the location format from 2 parameters : latitude and longitude to
      one location string "<latitude>,<longitude>" like "38.915645,-77.220796".

--- cloudify_cli/cli/helptexts.py
+++ cloudify_cli/cli/helptexts.py
@@ -323,6 +323,6 @@ WAIT_AFTER_FAIL = 'When a task fails, wait this many seconds for ' \
                   'already-running tasks to return'
 RESET_OPERATIONS = 'Reset operations in started state, so that they are '\
                    'run again unconditionally'
-LATITUDE = "The latitude of the site's location"
-LONGITUDE = "The longitude of the site's location"
+LOCATION = "The location of the site, expected format: latitude,longitude " \
+           "such as 32.071072,34.787274"
 NEW_NAME = "The new name of the {0}"
