7ac8f686cdfe7898c98ff359a497ed2d8365e309
OHAD <ohadbaruch1@gmail.com>
2019-06-02 13:32:47 +0300

    Adding new test and updating existing one

--- cloudify_cli/tests/commands/test_cluster.py
+++ cloudify_cli/tests/commands/test_cluster.py
@@ -19,58 +19,72 @@ import mock
 from requests.exceptions import ConnectionError
 
 from .test_base import CliCommandTest
-from cloudify_rest_client.manager import ManagerItem
+from cloudify_rest_client.manager import ManagerItem, RabbitMQBrokerItem
 from cloudify_cli.tests.cfy import ClickInvocationException
 
 
 class ClusterTest(CliCommandTest):
     MANAGERS_LIST = [
-            ManagerItem(
-                {
-                    'id': '0',
-                    'hostname': 'hostname_1',
-                    'private_ip': '1.2.3.4',
-                    'public_ip': '2.2.3.4',
-                    'version': '5.0',
-                    'edition': 'premium',
-                    'distribution': 'centos',
-                    'distro_release': 'core',
-                    'fs_sync_node_id': 'hgujriewgthuiyenfjk'
-                }
-            ),
-            ManagerItem(
-                {
-                    'id': '1',
-                    'hostname': 'hostname_2',
-                    'private_ip': '1.2.3.5',
-                    'public_ip': '2.2.3.5',
-                    'version': '5.0',
-                    'edition': 'premium',
-                    'distribution': 'centos',
-                    'distro_release': 'core',
-                    'fs_sync_node_id': 'hgujriewgthuiyenfjk'
-                }
-            ),
-            ManagerItem(
-                {
-                    'id': '2',
-                    'hostname': 'hostname_3',
-                    'private_ip': '1.2.3.6',
-                    'public_ip': '2.2.3.6',
-                    'version': '5.0',
-                    'edition': 'premium',
-                    'distribution': 'centos',
-                    'distro_release': 'core',
-                    'fs_sync_node_id': 'hgujriewgthuiyenfjk'
-                }
-            )
-        ]
+        ManagerItem(
+            {
+                'id': '0',
+                'hostname': 'hostname_1',
+                'private_ip': '1.2.3.4',
+                'public_ip': '2.2.3.4',
+                'version': '5.0',
+                'edition': 'premium',
+                'distribution': 'centos',
+                'distro_release': 'core',
+                'fs_sync_node_id': 'hgujriewgthuiyenfjk'
+            }
+        ),
+        ManagerItem(
+            {
+                'id': '1',
+                'hostname': 'hostname_2',
+                'private_ip': '1.2.3.5',
+                'public_ip': '2.2.3.5',
+                'version': '5.0',
+                'edition': 'premium',
+                'distribution': 'centos',
+                'distro_release': 'core',
+                'fs_sync_node_id': 'hgujriewgthuiyenfjk'
+            }
+        ),
+        ManagerItem(
+            {
+                'id': '2',
+                'hostname': 'hostname_3',
+                'private_ip': '1.2.3.6',
+                'public_ip': '2.2.3.6',
+                'version': '5.0',
+                'edition': 'premium',
+                'distribution': 'centos',
+                'distro_release': 'core',
+                'fs_sync_node_id': 'hgujriewgthuiyenfjk'
+            }
+        )
+    ]
+    BROKERS_LIST = [
+        RabbitMQBrokerItem(
+            {
+                'name': 'broker1',
+                'host': '3.2.3.4',
+                'port': '15671',
+                'params': {},
+                'ca_cert_content': 'CA CONTENT',
+                'networks': {'default': '3.2.3.4'}
+            }
+        )
+    ]
 
     def setUp(self):
         super(ClusterTest, self).setUp()
         self.client.manager.get_status = mock.MagicMock()
         self.client.maintenance_mode.status = mock.MagicMock()
         self.client.manager.get_managers = mock.MagicMock()
+        self.client.manager.get_brokers = mock.MagicMock(
+            return_value=self.BROKERS_LIST)
         self.client.manager.get_managers().items = self.MANAGERS_LIST
 
     def test_list_nodes(self):
@@ -113,6 +127,72 @@ class ClusterTest(CliCommandTest):
         ]
         outcome = self.invoke('cfy cluster status')
         supposed_to_be_in_list = [
+            'Active',
+            'Offline',
+            'hostname_1',
+            '1.2.3.5',
+            'broker1',
+            '15671',
+            '3.2.3.4'
+        ]
+        not_supposed_to_be_in_list = [
+            'Service-1',
+            'Service-2',
+            'Service-3',
+            'Service-4',
+            'Service-BlaBla',
+            'N/A',
+            'down',
+            'remote',
+            'running',
+            'id',
+            'fs_sync_node_id'
+        ]
+        for supposed_to_be_in in supposed_to_be_in_list:
+            self.assertIn(supposed_to_be_in, outcome.output)
+        for not_supposed_to_be_in in not_supposed_to_be_in_list:
+            self.assertNotIn(not_supposed_to_be_in, outcome.output)
+
+    def test_list_nodes_verbose(self):
+        self.use_manager()
+
+        self.client.manager.get_status.side_effect = [
+            {
+                'services': [
+                    {
+                        'instances': [{'state': 'running'}],
+                        'display_name': 'Service-1'
+                    },
+                    {
+                        'instances': [{'state': 'remote'}],
+                        'display_name': 'Service-2'
+                    },
+                    {
+                        'instances': [{'state': 'down'}],
+                        'display_name': 'Service-3'
+                    },
+                    {
+                        'instances': [{'state': 'running'}],
+                        'display_name': 'Service-4'
+                    }
+                ]
+            },
+            ConnectionError,
+            {
+                'services': [
+                    {
+                        'instances': [{'state': 'running'}],
+                        'display_name': 'Service-BlaBla'
+                    },
+                    {
+                        'instances': [{'state': 'down'}],
+                        'display_name': 'Service-1'
+                    }
+                ]
+            }
+        ]
+        outcome = self.invoke('cfy cluster status -v')
+        supposed_to_be_in_list = [
             'Service-1',
             'Service-2',
             'Service-3',
@@ -125,7 +205,10 @@ class ClusterTest(CliCommandTest):
             'Offline',
             'hostname_1',
             '1.2.3.5',
-            'N/A'
+            'N/A',
+            'broker1',
+            '15671',
+            '3.2.3.4'
         ]
         for supposed_to_be_in in supposed_to_be_in_list:
             self.assertIn(supposed_to_be_in, outcome.output)
