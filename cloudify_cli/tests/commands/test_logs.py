8629b919373d2b2e6aeee7ddf9d511892df78a8e
Yoni Itzhak <yoni@cloudify.co>
2020-01-12 14:27:10 +0200

    CY-2163 updating `cfy logs` command (#1035)
    * Updating `cfy logs download` command to match the new cluster attribute and download logs from all cluster nodes.

--- cloudify_cli/tests/commands/test_logs.py
+++ cloudify_cli/tests/commands/test_logs.py
@@ -8,30 +8,22 @@ class LogsTest(CliCommandTest):
         profile = self.use_manager(
             ssh_user=None, ssh_port=None, ssh_key_path=None)
         ssh.profile = profile
-        self.invoke('cfy logs download',
-                    'Manager `ssh_user` is not set '
-                    'in Cloudify CLI settings')
+        self.invoke('cfy logs download', ssh.SSH_ERR.format('user'))
 
     def test_with_no_key(self):
         profile = self.use_manager(ssh_key_path=None)
         ssh.profile = profile
-        self.invoke('cfy logs download',
-                    'Manager `ssh_key` is not set '
-                    'in Cloudify CLI settings')
+        self.invoke('cfy logs download', ssh.SSH_ERR.format('key'))
 
     def test_with_no_user(self):
         profile = self.use_manager(ssh_user=None)
         ssh.profile = profile
-        self.invoke('cfy logs download',
-                    'Manager `ssh_user` is not set '
-                    'in Cloudify CLI settings')
+        self.invoke('cfy logs download', ssh.SSH_ERR.format('user'))
 
     def test_with_no_port(self):
         profile = self.use_manager(ssh_port=None)
         ssh.profile = profile
-        self.invoke('cfy logs download',
-                    'Manager `ssh_port` is not set '
-                    'in Cloudify CLI settings')
+        self.invoke('cfy logs download', ssh.SSH_ERR.format('port'))
 
     def test_purge_no_force(self):
         profile = self.use_manager()
@@ -44,41 +36,42 @@ class LogsTest(CliCommandTest):
     def __side_effect_mock_function__(self, *args, **kwargs):
         pass
 
-    @patch('cloudify_cli.ssh.get_file_from_manager')
-    @patch('cloudify_cli.ssh.run_command_on_manager')
+    @patch('cloudify_cli.ssh.get_file_from_host')
+    @patch('cloudify_cli.ssh.run_command_on_host')
     @patch('cloudify_cli.commands.logs._archive_logs',
            return_value='../resources/mocks/mock_tar.tar')
     def test_download_with_no_output_path(self,
                                           _archive_logs_mock,
-                                          run_command_on_manager_mock,
-                                          get_file_from_manager_mock):
+                                          run_command_on_host_mock,
+                                          get_file_from_host_mock):
         profile = self.use_manager()
         ssh.profile = profile
-        run_command_on_manager_mock.side_effect = \
+        run_command_on_host_mock.side_effect = \
             self.__side_effect_mock_function__
-        get_file_from_manager_mock.side_effect = \
+        get_file_from_host_mock.side_effect = \
             self.__side_effect_mock_function__
         outcome = self.invoke('cfy logs download')
         self.assertNotIn('Downloading archive to: None', outcome.logs)
 
-    @patch('cloudify_cli.ssh.get_file_from_manager')
-    @patch('cloudify_cli.ssh.run_command_on_manager')
+    @patch('cloudify_cli.ssh.get_file_from_host')
+    @patch('cloudify_cli.ssh.run_command_on_host')
     @patch('cloudify_cli.commands.logs._archive_logs',
            return_value='../resources/mocks/mock_tar.tar')
     def test_download_with_no_output_path_all_nodes(
             self,
             _archive_logs_mock,
-            run_command_on_manager_mock,
-            get_file_from_manager_mock):
-        cluster = [
+            run_command_on_host_mock,
+            get_file_from_host_mock):
+        cluster = {'manager': [
             {"manager_ip": "10.0.0.1", "ssh_user": "test", "ssh_key": "key"},
             {"manager_ip": "10.0.0.2", "ssh_user": "test", "ssh_key": "key"}
-        ]
+        ]}
+
         profile = self.use_manager(cluster=cluster)
         ssh.profile = profile
-        run_command_on_manager_mock.side_effect = \
+        run_command_on_host_mock.side_effect = \
             self.__side_effect_mock_function__
-        get_file_from_manager_mock.side_effect = \
+        get_file_from_host_mock.side_effect = \
             self.__side_effect_mock_function__
         outcome = self.invoke('cfy logs download --all-nodes')
         self.assertNotIn('Downloading archive to: None', outcome.logs)
