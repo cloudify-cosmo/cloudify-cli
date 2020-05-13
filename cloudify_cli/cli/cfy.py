70fc63960838b02066b5ec00392cdc826b2da474
Mateusz Neumann <mateumann@gmail.com>
2020-05-13 14:34:25 +0200

    CY-2725 Filtering events delete by timestamp range (#1081)
    This patch is accompanied by the other one in cloudify-manager where
    deletion is actually performed.
    
    * Add `--from` and `--to` options to the `cfy events delelete` command.
    
    * Add filtering for events list by timestamp range
    
    * Add `--from` and `--to` options to the `cfy events list` command.
    
    * Add listing of events prior to deletion
    
    * Two additional parameters added to `cfy events delete`:
     `--list-before` list/store events before deleting them.
     `-o, --output-path <PATH>`  save listed events to a specified file
    
    * Verbose end of period definition for events delete (`--before`)
    ```cfy events delete --before "3 months" test-deployment```
    
    * Write deleted events description to output file
    
    * Don't list deleted events to stdout
    
    If `--list-before` flag is enabled and `--output-path` parameters is
    added to the `cfy events delete` command, the listing before delation
    happens on cli side.  If `--list-before` is enabled but `--output-path`
    is not, the listing (and storing) of deleted events happens on
    manager's side.
    
    * Use datetime instead of strings for dates
    
    * Add test_delete_events_timeperiod

--- cloudify_cli/cli/cfy.py
+++ cloudify_cli/cli/cfy.py
@@ -22,6 +22,8 @@ import StringIO
 import warnings
 import traceback
 from functools import wraps
+import datetime
+import re
 
 import click
 from cloudify_rest_client.constants import VisibilityState
@@ -82,6 +84,34 @@ class MutuallyExclusiveOption(click.Option):
             )
         return super(MutuallyExclusiveOption, self).handle_parse_result(
             ctx, opts, args)
+
+
+def _parse_relative_datetime(ctx, param, rel_datetime):
+    """Change relative time (ago) to a valid timestamp"""
+    if not rel_datetime:
+        return None
+    parsed = re.findall(r"(\d+) (seconds?|minutes?|hours?|days?|weeks?"
+                        "|months?|years?) ?(ago)?",
+                        rel_datetime)
+    if not parsed or len(parsed[0]) < 2:
+        return None
+    number = int(parsed[0][0])
+    period = parsed[0][1]
+    if period[-1] != u's':
+        period += u's'
+    now = datetime.datetime.utcnow()
+    if period == u'years':
+        result = now.replace(year=now.year - number)
+    elif period == u'months':
+        if now.month > number:
+            result = now.replace(month=now.month - number)
+        else:
+            result = now.replace(month=now.month - number + 12,
+                                 year=now.year - 1)
+    else:
+        delta = datetime.timedelta(**{period: number})
+        result = now - delta
+    return result.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 
 
 def _format_version_data(version_data,
@@ -1409,6 +1439,67 @@ class Options(object):
             '--yaml-path',
             required=True,
             help=helptexts.PLUGIN_YAML_PATH)
+
+    @staticmethod
+    def from_datetime(required=False, mutually_exclusive_with=None,
+                      help=helptexts.FROM_DATETIME):
+        kwargs = {
+            'required': required,
+            'type': str,
+            'help': help,
+        }
+        if mutually_exclusive_with:
+            kwargs['cls'] = MutuallyExclusiveOption
+            kwargs['mutually_exclusive'] = mutually_exclusive_with
+        return click.option('from_datetime', '--from', **kwargs)
+
+    @staticmethod
+    def to_datetime(required=False, mutually_exclusive_with=None,
+                    help=helptexts.TO_DATETIME):
+        kwargs = {
+            'required': required,
+            'type': str,
+            'help': help,
+        }
+        if mutually_exclusive_with:
+            kwargs['cls'] = MutuallyExclusiveOption
+            kwargs['mutually_exclusive'] = mutually_exclusive_with
+        return click.option('to_datetime', '--to', **kwargs)
+
+    @staticmethod
+    def before(required=False,
+               mutually_exclusive_with=None,
+               help=helptexts.BEFORE):
+        kwargs = {
+            'required': required,
+            'type': str,
+            'callback': _parse_relative_datetime,
+            'expose_value': True,
+            'help': help,
+        }
+        if mutually_exclusive_with:
+            kwargs['cls'] = MutuallyExclusiveOption
+            kwargs['mutually_exclusive'] = mutually_exclusive_with
+        return click.option('--before', **kwargs)
+
+    @staticmethod
+    def store_before(default=False):
+        return click.option(
+            '--store-before',
+            is_flag=True,
+            default=default,
+            help=helptexts.STORE_BEFORE_DELETION
+        )
+
+    @staticmethod
+    def store_output_path():
+        return click.option(
+            '-o',
+            '--output-path',
+            required=False,
+            type=click.Path(file_okay=True, dir_okay=False),
+            help=helptexts.STORE_OUTPUT_PATH
+        )
 
 
 options = Options()
--- cloudify_cli/cli/helptexts.py
+++ cloudify_cli/cli/helptexts.py
@@ -318,3 +318,10 @@ WAIT_AFTER_FAIL = 'When a task fails, wait this many seconds for ' \
                   'already-running tasks to return'
 RESET_OPERATIONS = 'Reset operations in started state, so that they are '\
                    'ran again unconditionally'
+
+FROM_DATETIME = "Beginning of a period"
+TO_DATETIME = "End of a period"
+BEFORE = "How long ago did the specified period ended"
+
+STORE_BEFORE_DELETION = "List and store events before deleting them"
+STORE_OUTPUT_PATH = "Store listed events to a specified file (cli side)"
