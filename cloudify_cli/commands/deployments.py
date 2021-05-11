0316117325e207e22263916f3d862824b02ed231
Mateusz Neumann <mateumann@gmail.com>
2021-05-11 13:41:03 +0200

    Use templating for display_name

--- cloudify_cli/commands/deployments.py
+++ cloudify_cli/commands/deployments.py
@@ -1159,13 +1159,9 @@ def groups_extend(deployment_group_name, deployment_id, count, filter_id,
         for deployment in client.deployments.list(
                 deployment_group_id=environments_group):
             if deployment.is_environment():
-                new_id = str(uuid.uuid4())
                 new_deployments.append({
-                    'id': new_id,
-                    'display_name': "{blueprint_id}-{uuid}".format(
-                        blueprint_id=deployment_group.default_blueprint_id,
-                        uuid=new_id,
-                    ),
+                    'id': '{uuid}',
+                    'display_name': '{blueprint_id}-{uuid}',
                     'labels': [{'csys-obj-parent': deployment.id}],
                 })
     group = client.deployment_groups.add_deployments(
