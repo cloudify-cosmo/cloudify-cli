a20023a304c91ac4e1a6d60c5e81d3d5f462f6f7
Mateusz Neumann <mateumann@gmail.com>
2021-10-27 19:03:59 +0200

    Also allow for an icon to be removed

--- cloudify_cli/commands/blueprints.py
+++ cloudify_cli/commands/blueprints.py
@@ -554,16 +554,22 @@ def summary(target_field, sub_field, logger, client, tenant_name,
 @blueprints.command(name='set-icon',
                     short_help="Set the blueprint's icon")
 @cfy.argument('blueprint-id')
-@cfy.options.blueprint_icon_path(required=True)
+@cfy.options.blueprint_icon_path()
 @cfy.assert_manager_active()
 @cfy.pass_client()
 @cfy.pass_logger
 def set_icon(blueprint_id, icon_path, logger, client):
     """Set an icon which will be used to describe/identify the blueprint."""
-    status_codes = [400, 401, 403, 404]
+    status_codes = [400, 403, 404]
     with prettify_client_error(status_codes, logger):
-        client.blueprints.upload_icon(blueprint_id, icon_path)
-        logger.info('Blueprint `{0}` has a new icon set.'.format(blueprint_id))
+        if icon_path:
+            client.blueprints.upload_icon(blueprint_id, icon_path)
+            logger.info('Blueprint `{0}` has a new icon set.'
+                        .format(blueprint_id))
+        else:
+            client.blueprints.remove_icon(blueprint_id)
+            logger.info('Blueprint `{0}` has its icon removed.'
+                        .format(blueprint_id))
 
 
 @blueprints.group(name='labels',
