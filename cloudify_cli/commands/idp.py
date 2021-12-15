096f1bf4ccecd3c2cbf348de823c53f65d0e0a3c
Madda <nobody@nowhere.local>
2021-12-15 14:25:29 +0100

    RD-1050 Identity provider CLI

---
+++ cloudify_cli/commands/idp.py
@@ -0,0 +1,18 @@
+from ..cli import cfy
+
+
+@cfy.group(name='identity')
+@cfy.options.common_options
+@cfy.assert_manager_active()
+def idp():
+    """Identity provider commands.
+    """
+    pass
+
+
+@idp.command(name='get',
+             short_help='Get the current identity provider for the manager.')
+@cfy.pass_client()
+@cfy.pass_logger
+def get(client, logger):
+    logger.info(client.idp.get())
