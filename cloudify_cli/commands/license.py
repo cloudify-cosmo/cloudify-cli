aec071ed87815ecb0f11c814ed9c4a80c3ddeb56
Michael Glukhman <michaelg@cloudify.co>
2021-12-16 12:55:14 +0200

    RD-3558 Add environments list+count

--- cloudify_cli/commands/license.py
+++ cloudify_cli/commands/license.py
@@ -1,11 +1,14 @@
 import os
 
 from cloudify_cli.cli import cfy
+from cloudify_cli.logger import output
 from cloudify_cli.table import print_data
 from cloudify_cli.exceptions import CloudifyValidationError
 
 LICENSE_COLUMN = ['customer_id', 'expiration_date', 'license_edition', 'trial',
                   'cloudify_version', 'capabilities', 'expired']
+ENVIRONMENT_COLUMNS = ['id', 'display_name', 'tenant_name']
+ENVIRONMENT_LABELS = {'id': 'deployment_id', 'display_name': 'deployment_name'}
 
 
 @cfy.group(name='license')
@@ -63,3 +66,58 @@ def upload(logger, client, license_path):
                                       format(license_path))
     client.license.upload(license_path)
     logger.info('Cloudify license successfully uploaded.')
+
+
+@cfy.group(name='environments')
+@cfy.options.common_options
+@cfy.assert_manager_active()
+def environments():
+    """Handle licensed environments on the manager
+    """
+    pass
+
+
+@environments.command(name='list',
+                      short_help='List all licensed environments')
+@cfy.options.sort_by()
+@cfy.options.descending
+@cfy.options.pagination_offset
+@cfy.options.pagination_size
+@cfy.options.common_options
+@cfy.pass_client()
+@cfy.pass_logger
+@cfy.options.extended_view
+def environments_list(sort_by,
+                      descending,
+                      pagination_offset,
+                      pagination_size,
+                      logger,
+                      client):
+    """List all licensed environments on the manager.
+    """
+    logger.info('Listing all licensed environments...')
+
+    environments = client.deployments.list(sort=sort_by,
+                                           is_descending=descending,
+                                           _offset=pagination_offset,
+                                           _size=pagination_size,
+                                           _all_tenants=True,
+                                           _environments_only=True)
+    print_data(ENVIRONMENT_COLUMNS, environments, 'Environments:',
+               labels=ENVIRONMENT_LABELS)
+    logger.info('Showing %d of %d environments',
+                len(environments), environments.metadata.pagination.total)
+
+
+@environments.command(name='count',
+                      short_help='Print the count of licensed environments')
+@cfy.options.common_options
+@cfy.pass_logger
+@cfy.pass_client()
+def environments_count(logger, client):
+    """Print the count of licensed environments on the manager.
+    """
+    environments = client.deployments.list(_all_tenants=True,
+                                           _get_all_results=True,
+                                           _environments_only=True)
+    output('Licensed environments count: {}'.format(len(environments)))
