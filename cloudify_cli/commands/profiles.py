d21ccb25880a9bd33f1e9899143cc45953f63bc8
Madda <nobody@nowhere.local>
2022-03-24 12:46:35 +0100

    Honour skip_credentials_validation flag on set

--- cloudify_cli/commands/profiles.py
+++ cloudify_cli/commands/profiles.py
@@ -389,11 +389,12 @@ def set_cmd(profile_name,
             "You must supply at least one of the following:  "
             "profile name, username, password, token, tenant, "
             "ssl, rest certificate, ssh user, ssh key, ssh port, kerberos env")
-    env.check_configured_auth(
-        credentials=(manager_username, manager_password),
-        token=manager_token,
-        kerberos_env=kerberos_env,
-    )
+    if not skip_credentials_validation:
+        env.check_configured_auth(
+            credentials=(manager_username, manager_password),
+            token=manager_token,
+            kerberos_env=kerberos_env,
+        )
 
     old_name = None
     if profile_name:
