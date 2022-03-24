ff23e961558d0babde25693b566089d348a416bf
Madda <nobody@nowhere.local>
2022-03-24 16:12:53 +0100

    RD-4414 Test token endpoint CLI

--- cloudify_cli/tests/commands/test_tokens.py
+++ cloudify_cli/tests/commands/test_tokens.py
@@ -1,21 +1,73 @@
-from mock import MagicMock
+from mock import Mock
 
-from .test_base import CliCommandTest
+from cloudify_rest_client.tokens import Token
+
+from cloudify_cli.tests.commands.test_base import CliCommandTest
+from cloudify_cli.tests.commands.mocks import MockListResponse
+
+
+TOKEN1 = {'id': 'abc123', 'role': 'someroleorother', 'username': 'user1',
+          'description': 'areallyusefultoken',
+          'value': 'ctok-something-willbehiddenbyserver'}
+TOKEN2 = {'id': 'xyz4576', 'role': 'adifferentrole', 'username': 'user2',
+          'description': 'trokenboken', 'last_used': 'A Long Time Ago',
+          'expiration_date': 'Very very soon',
+          'value': 'ctok-other-alsohiddenvalue'}
 
 
 class TokensTest(CliCommandTest):
 
     def setUp(self):
         super(TokensTest, self).setUp()
-        self.client.tokens.get = MagicMock()
+        self.client.tokens.get = Mock()
+        self.client.tokens.list = Mock()
+        self.client.tokens.delete = Mock()
         self.use_manager()
 
-    def test_tokens_command(self):
-        self.use_manager()
-        self.invoke('cfy tokens get')
+    def test_old_command_error(self):
+        # Make sure we give a helpful error on the old style token retrieval
+        self.invoke('cfy tokens get', err_str_segment='`cfy tokens create`')
 
-    def test_tokens_wrong_argument(self):
-        outcome = self.invoke('cfy tokens get -t',
-                              err_str_segment='2',
-                              exception=SystemExit)
-        self.assertIn('Error: no such option: -t', outcome.output)
+    def _check_output(self, tokens, output, expect_users=False,
+                      expect_count=True):
+        for token in tokens:
+            for key in ['id', 'role', 'description', 'value',
+                        'expiration_date', 'last_used']:
+                if token.get(key):
+                    assert token.get(key) in output
+
+            if expect_users:
+                assert token['username'] in output
+            else:
+                assert token['username'] not in output
+
+        if expect_count:
+            assert '{0} of {0}'.format(len(tokens)) in output
+
+    def test_get_token(self):
+        token = Token(TOKEN1)
+        self.client.tokens.get.return_value = token
+        output = self.invoke('cfy tokens get {}'.format(token['id'])).output
+        self.client.tokens.get.assert_called_once_with(token['id'])
+        self._check_output([token], output, expect_count=False)
+
+    def test_delete_token(self):
+        token_id = 'killthistoken'
+        self.invoke('cfy tokens delete {}'.format(token_id))
+        self.client.tokens.delete.assert_called_once_with(token_id)
+
+    def test_list_single_user(self):
+        tokens = MockListResponse(items=[Token(TOKEN1)])
+        tokens.metadata.pagination.total = len(tokens)
+        self.client.tokens.list.return_value = tokens
+        output = self.invoke('cfy tokens list').output
+        self.client.tokens.list.assert_called_once_with()
+        self._check_output(tokens.items, output)
+
+    def test_list_multi_user(self):
+        tokens = MockListResponse(items=[Token(TOKEN1), Token(TOKEN2)])
+        tokens.metadata.pagination.total = len(tokens)
+        self.client.tokens.list.return_value = tokens
+        output = self.invoke('cfy tokens list').output
+        self.client.tokens.list.assert_called_once_with()
+        self._check_output(tokens.items, output, expect_users=True)
