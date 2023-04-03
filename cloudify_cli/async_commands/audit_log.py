707b2b735523f9392811afbb96f60f9791bde602
Mateusz Neumann <mateumann@gmail.com>
2023-04-03 10:56:17 +0200

    Wrap aiohttp ClientError with CloudifyCliError (#1473)
    This is a cherry-pick of https://github.com/cloudify-cosmo/cloudify-cli/pull/1472

--- cloudify_cli/async_commands/audit_log.py
+++ cloudify_cli/async_commands/audit_log.py
@@ -1,6 +1,8 @@
 import asyncio
 import json
 
+import aiohttp.client_exceptions
+
 from cloudify_cli.exceptions import CloudifyCliError
 from cloudify_cli.logger import get_global_json_output
 
@@ -34,12 +36,15 @@ async def _stream_logs(creator_name,
                                             creator_name=creator_name,
                                             execution_id=execution_id,
                                             since=since)
-    async for data in response.content:
-        for audit_log in _streamed_audit_log(data):
-            if get_global_json_output():
-                print(audit_log)
-            else:
-                print(_format_audit_log(audit_log))
+    try:
+        async for data in response.content:
+            for audit_log in _streamed_audit_log(data):
+                if get_global_json_output():
+                    print(audit_log)
+                else:
+                    print(_format_audit_log(audit_log))
+    except aiohttp.client_exceptions.ClientError as e:
+        raise CloudifyCliError(f'Error getting audit log stream: {e}') from e
 
 
 def _streamed_audit_log(data):
