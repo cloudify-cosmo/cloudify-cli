397aaa408c277763306ca9311f6356879e71c532
opencm <limor@gigaspaces.com>
2023-06-22 06:51:36 +0000

    Bump version

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -10,7 +10,7 @@ export REPO=$5
 export BRANCH=$6
 
 # These env vars are being updated by the bump version process
-export CORE_TAG_NAME="7.0.1.dev1"
+export CORE_TAG_NAME="7.0.1"
 export CORE_BRANCH="7.0.1-build"
 
 # Linux Preperation
