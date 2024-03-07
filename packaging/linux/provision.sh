e7dc554789cc7cb2db2539787618e401fda6759f
opencm <limor@gigaspaces.com>
2024-03-07 15:20:17 +0000

    Bump version

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -10,7 +10,7 @@ export REPO=$5
 export BRANCH=$6
 
 # These env vars are being updated by the bump version process
-export CORE_TAG_NAME="7.0.4.dev1"
+export CORE_TAG_NAME="7.0.4"
 export CORE_BRANCH="7.0.4-build"
 
 # Linux Preperation
--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ packages = [
 
 setup(
     name='cloudify',
-    version='7.0.4.dev1',
+    version='7.0.4',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=packages,
